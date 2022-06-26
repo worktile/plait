e0965399a2e7bb884e48880de2103f7d3f9a88b4
pubuzhixing8 <pubuzhixing@gmail.com>
2022-06-26 18:13:13 +0800

    feat(app): storage board event data

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { withMindmap } from 'mindmap';
-import { PlaitElement } from 'plait';
+import { PlaitElement, PlaitBoardChangeEvent, Viewport } from 'plait';
 import { mockMindmapData } from '../mock/mindmap-data';
+
+const LOCAL_DATA_KEY = 'plait-board-change-data';
 
 @Component({
     selector: 'basic-board',
     template: `
-        <plait-board [plugins]="plugins" [value]="value"></plait-board>
+        <plait-board [plaitPlugins]="plugins" [plaitValue]="value" [plaitViewport]="viewport" (plaitChange)="change($event)"></plait-board>
     `
 })
 export class BasicBoardComponent implements OnInit {
@@ -14,5 +16,26 @@ export class BasicBoardComponent implements OnInit {
 
     value: PlaitElement[] = [mockMindmapData];
 
-    ngOnInit(): void {}
+    viewport!: Viewport;
+
+    ngOnInit(): void {
+        const data = this.getLocalData() as PlaitBoardChangeEvent;
+        if (data) {
+            this.value = data.children;
+            this.viewport = data.viewport;
+        }
+    }
+
+    change(event: PlaitBoardChangeEvent) {
+        this.setLocalData(JSON.stringify(event));
+    }
+
+    setLocalData(data: string) {
+        localStorage.setItem(`${LOCAL_DATA_KEY}`, data);
+    }
+
+    getLocalData() {
+        const data = localStorage.getItem(`${LOCAL_DATA_KEY}`);
+        return data ? JSON.parse(data) : null;
+    }
 }
