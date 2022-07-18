6d650cf715c9f394964446db2c77af737573b4be
pubuzhixing8 <pubuzhixing@gmail.com>
2022-07-18 15:50:06 +0800

    feat: add 18 colors

--- packages/mindmap/src/constants/node.ts
+++ packages/mindmap/src/constants/node.ts
@@ -4,4 +4,23 @@ export const NODE_FILL = '#FFFFFF';
 export const ROOT_NODE_FILL = '#F5F5F5';
 export const ROOT_TOPIC_FONT_SIZE = 18;
 
-export const STORKE_COLORS = ['#868AF6', '#56ABFB', '#FF9F73', '#3DD1AE', '#F6C659'];
+export const STORKE_COLORS = [
+    '#56ABFB',
+    '#5DCFFF',
+    '#5BE2EE',
+    '#40E0C3',
+    '#3DD1AE',
+    '#A9E072',
+    '#84E17E',
+    '#73D897',
+    '#F4D66D',
+    '#F6C659',
+    '#FF9F73',
+    '#FA8888',
+    '#FB7894',
+    '#FB7FB7',
+    '#F693E7',
+    '#D287F8',
+    '#9A7EF4',
+    '#868AF6'
+];
