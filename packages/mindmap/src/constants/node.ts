35950a2c0755cc0dcd02dd9436bea24d2832228e
pubuzhixing8 <pubuzhixing@gmail.com>
2022-07-21 10:33:59 +0800

    feat(mindmap): support branch colors

--- packages/mindmap/src/constants/node.ts
+++ packages/mindmap/src/constants/node.ts
@@ -5,22 +5,22 @@ export const ROOT_NODE_FILL = '#F5F5F5';
 export const ROOT_TOPIC_FONT_SIZE = 18;
 
 export const COLORS = [
+    '#868AF6',
+    '#D287F8',
+    '#FB7FB7',
+    '#FA8888',
+    '#F6C659',
+    '#73D897',
+    '#A9E072',
+    '#5BE2EE',
     '#56ABFB',
     '#5DCFFF',
-    '#5BE2EE',
     '#40E0C3',
-    '#3DD1AE',
-    '#A9E072',
     '#84E17E',
-    '#73D897',
     '#F4D66D',
-    '#F6C659',
     '#FF9F73',
-    '#FA8888',
     '#FB7894',
-    '#FB7FB7',
     '#F693E7',
-    '#D287F8',
     '#9A7EF4',
-    '#868AF6'
+    '#AAAAAA'
 ];
