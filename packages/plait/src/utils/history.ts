ec23b285af91217569a682b41b06468863cd3c92
huanhuanwa <44698191+huanhuanwa@users.noreply.github.com>
2022-09-02 09:24:43 +0800

    feat(core): support undo and redo#WIK-8473 (#37)

---
+++ packages/plait/src/utils/history.ts
@@ -0,0 +1,36 @@
+import { PlaitOperation } from '../interfaces';
+
+/**
+ * Check whether to merge an operation into the previous operation.
+ */
+
+export const shouldMerge = (op: PlaitOperation, prev: PlaitOperation | undefined): boolean => {
+    if (op.type === 'set_viewport') {
+        return true;
+    }
+    return false;
+};
+
+/**
+ * Check whether an operation needs to be saved to the history.
+ */
+
+export const shouldSave = (op: PlaitOperation, prev: PlaitOperation | undefined): boolean => {
+    if (op.type === 'set_selection') {
+        return false;
+    }
+
+    return true;
+};
+
+/**
+ * Check whether an operation should clear the redos stack.
+ */
+
+export const shouldClear = (op: PlaitOperation): boolean => {
+    if (op.type === 'set_selection') {
+        return false;
+    }
+
+    return true;
+};
--- packages/richtext/src/plugins/with-marks.ts
+++ packages/richtext/src/plugins/with-marks.ts
@@ -1,4 +1,4 @@
-import isHotkey from 'is-hotkey';
+import { isHotkey } from 'is-hotkey';
 import { Editor, Text } from 'slate';
 import { RichtextEditor } from './richtext-editor';
 
