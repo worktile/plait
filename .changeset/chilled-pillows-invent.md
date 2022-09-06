750e194caa3679e177de7467b96a108463af498a
gzy <1056342711@qq.com>
2022-09-06 14:25:19 +0800

    feat(mindmap): redraw indented link

---
+++ .changeset/chilled-pillows-invent.md
@@ -0,0 +1,5 @@
+---
+'@plait/mindmap': patch
+---
+
+redraw indented link
--- packages/mindmap/src/draw/indented-link.ts
+++ packages/mindmap/src/draw/indented-link.ts
@@ -16,23 +16,30 @@ export function drawIndentedLink(roughSVG: RoughSVG, node: MindmapNode, child: M
         beginNode = node,
         endNode = child;
     const beginRectangle = getRectangleByNode(beginNode);
-    beginX = Math.round(beginNode.x + beginNode.width / 2);
-    beginY = Math.round(beginRectangle.y + beginRectangle.height);
-    endX = Math.round(endNode.x + endNode.hGap);
-    endY = hasUnderline ? Math.round(endNode.y + endNode.height - endNode.vGap) : Math.round(endNode.y + endNode.height / 2);
+    const endRectangle = getRectangleByNode(endNode);
 
-    const stroke = defaultStroke || getLinkLineColorByMindmapElement(child.origin);
-    const strokeWidth = child.origin.linkLineWidth ? child.origin.linkLineWidth : STROKE_WIDTH;
+    beginX = Math.round(beginNode.x + beginNode.width / 2);
+    beginY = node.y < child.y ? Math.round(beginRectangle.y + beginRectangle.height) : Math.round(beginRectangle.y);
+    endX = node.x < child.x ? Math.round(endNode.x + endNode.hGap) : Math.round(endNode.x + endNode.hGap + endRectangle.width);
+    endY = hasUnderline ? Math.round(endNode.y + endNode.height - endNode.vGap) : Math.round(endNode.y + endNode.height / 2);
+    //方位参数
+    let plusMinus = node.y < child.y ? (node.x < child.x ? [1, 1] : [-1, 1]) : node.x < child.x ? [1, -1] : [-1, -1];
 
     let curve: Point[] = [
         [beginX, beginY],
-        [beginX, endY],
-        [beginX, endY],
-        [beginX, endY],
-        [endX, endY],
-        [endX, endY],
-        hasUnderline ? [endX + (endNode.width - endNode.hGap * 2), endY] : [endX, endY]
+        [beginX, endY - (endNode.hGap * 3 * plusMinus[1]) / 5],
+        [beginX, endY - (endNode.hGap * 3 * plusMinus[1]) / 5],
+        [beginX, endY - (endNode.hGap * 3 * plusMinus[1]) / 5],
+        [beginX, endY - (endNode.hGap * plusMinus[1]) / 5],
+        [beginX + (endNode.hGap * plusMinus[0]) / 4, endY],
+        [endX - (endNode.vGap * plusMinus[0]) / 4, endY],
+        [endX - (endNode.vGap * plusMinus[0]) / 4, endY],
+        [endX - (endNode.vGap * plusMinus[0]) / 4, endY],
+        hasUnderline ? [endX + (endNode.width - endNode.hGap * 2) * plusMinus[0], endY] : [endX, endY]
     ];
+
+    const stroke = defaultStroke || getLinkLineColorByMindmapElement(child.origin);
+    const strokeWidth = child.origin.linkLineWidth ? child.origin.linkLineWidth : STROKE_WIDTH;
 
     const points = pointsOnBezierCurves(curve);
     return roughSVG.curve(points as any, { stroke, strokeWidth });
