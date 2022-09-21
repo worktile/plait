f94fc470ce6290115c4d0c091224e44a2c5be9bb
ajie <m18210870360@163.com>
2022-09-21 16:07:25 +0800

    fix: dnd idented layout style of mindmap #WIK-8625

--- packages/mindmap/src/draw/indented-link.ts
+++ packages/mindmap/src/draw/indented-link.ts
@@ -1,11 +1,10 @@
 import { pointsOnBezierCurves } from 'points-on-curve';
 import { RoughSVG } from 'roughjs/bin/svg';
 import { MindmapNodeShape, STROKE_WIDTH } from '../constants';
-import { MindmapElement } from '../interfaces';
 import { MindmapNode } from '../interfaces/node';
 import { getLinkLineColorByMindmapElement } from '../utils/colors';
 import { Point } from '@plait/core';
-import { getNodeShapeByElement, getRectangleByNode, isChildRight, isChildUp } from '../utils';
+import { getNodeShapeByElement, getRectangleByNode, isChildUp } from '../utils';
 
 export function drawIndentedLink(
     roughSVG: RoughSVG,
@@ -14,7 +13,7 @@ export function drawIndentedLink(
     defaultStroke: string | null = null,
     needDrawUnderline = true
 ) {
-    const hasUnderline = (getNodeShapeByElement(child.origin) as MindmapNodeShape) === MindmapNodeShape.underline;
+    const isUnderlineShap = (getNodeShapeByElement(child.origin) as MindmapNodeShape) === MindmapNodeShape.underline;
     let beginX,
         beginY,
         endX,
@@ -27,7 +26,7 @@ export function drawIndentedLink(
     beginX = beginNode.x + beginNode.width / 2;
     beginY = isChildUp(node, child) ? beginRectangle.y : beginRectangle.y + beginRectangle.height;
     endX = node.left ? endNode.x + endNode.hGap + endRectangle.width : endNode.x + endNode.hGap;
-    endY = hasUnderline ? endNode.y + endNode.height - endNode.vGap : endNode.y + endNode.height / 2;
+    endY = isUnderlineShap ? endNode.y + endNode.height - endNode.vGap : endNode.y + endNode.height / 2;
     //根据位置，设置正负参数
     let plusMinus = isChildUp(node, child) ? (node.left ? [-1, -1] : [1, -1]) : node.left ? [-1, 1] : [1, 1];
 
@@ -39,9 +38,9 @@ export function drawIndentedLink(
         [beginX, endY - (endNode.hGap * plusMinus[1]) / 5],
         [beginX + (endNode.hGap * plusMinus[0]) / 4, endY],
         [beginX + (endNode.hGap * plusMinus[0] * 3) / 5, endY],
-        hasUnderline && needDrawUnderline ? [endX + (endNode.width - endNode.hGap * 2) * plusMinus[0], endY] : [endX, endY],
-        hasUnderline && needDrawUnderline ? [endX + (endNode.width - endNode.hGap * 2) * plusMinus[0], endY] : [endX, endY],
-        hasUnderline && needDrawUnderline ? [endX + (endNode.width - endNode.hGap * 2) * plusMinus[0], endY] : [endX, endY]
+        isUnderlineShap && needDrawUnderline ? [endX + (endNode.width - endNode.hGap * 2) * plusMinus[0], endY] : [endX, endY],
+        isUnderlineShap && needDrawUnderline ? [endX + (endNode.width - endNode.hGap * 2) * plusMinus[0], endY] : [endX, endY],
+        isUnderlineShap && needDrawUnderline ? [endX + (endNode.width - endNode.hGap * 2) * plusMinus[0], endY] : [endX, endY]
     ];
 
     const stroke = defaultStroke || getLinkLineColorByMindmapElement(child.origin);
