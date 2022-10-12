e6daf809beceabc57f053d940659fa818465c9c4
huanhuanwa <2323666215@qq.com>
2022-10-12 15:43:22 +0800

    fix: fix resolve conflict error

--- packages/mindmap/src/draw/link.ts
+++ packages/mindmap/src/draw/link.ts
@@ -57,7 +57,6 @@ export function drawLink(
 
     const stroke = defaultStroke || getLinkLineColorByMindmapElement(child.origin);
     const strokeWidth = child.origin.linkLineWidth ? child.origin.linkLineWidth : STROKE_WIDTH;
-    const layout = MindmapQueries.getCorrectLayoutByElement(node.origin) as MindmapLayoutType;
     if (endNode.origin.isRoot) {
         if (layout === MindmapLayoutType.left || isStandardLayout(layout)) {
             endX -= strokeWidth;
