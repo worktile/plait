321f7d1cb419c55d0111151a245ebf48eae12170
huanhuanwa <2323666215@qq.com>
2022-10-12 13:48:31 +0800

    fix: adjust correctLogicLayoutNode #WIK-8835

--- packages/mindmap/src/transforms/set-mindmap-layout.ts
+++ packages/mindmap/src/transforms/set-mindmap-layout.ts
@@ -1,6 +1,5 @@
 import { PlaitBoard, Path, PlaitNode, Transforms } from '@plait/core';
-import { isIndentedLayout, isLogicLayout, MindmapLayoutType } from '@plait/layouts';
-import { getAvailableSubLayoutsByLayoutDirections, getBranchDirectionsByLayouts } from '../utils';
+import { isHorizontalLogicLayout, isVerticalLogicLayout, MindmapLayoutType } from '@plait/layouts';
 
 export const setMindmapLayout = (board: PlaitBoard, layout: MindmapLayoutType, path: Path) => {
     correctLogicLayoutNode(board, layout, path);
@@ -9,22 +8,18 @@ export const setMindmapLayout = (board: PlaitBoard, layout: MindmapLayoutType, p
 
 const correctLogicLayoutNode = (board: PlaitBoard, layout: MindmapLayoutType, path: Path) => {
     const node = PlaitNode.get(board, path);
-    if (node && !isIndentedLayout(layout)) {
-        let mindmapLayout = layout;
-        if (mindmapLayout === MindmapLayoutType.standard) {
-            mindmapLayout = MindmapLayoutType.right;
-        }
-        const branchLayouts = getBranchDirectionsByLayouts([mindmapLayout]);
-        const availableSubLayouts = getAvailableSubLayoutsByLayoutDirections(branchLayouts);
+    if (node && layout) {
         node.children?.forEach((value: PlaitNode, index) => {
-            // 只修正不合法的逻辑布局
-            if (value.layout && isLogicLayout(value.layout)) {
-                if (availableSubLayouts.length && availableSubLayouts.findIndex(item => item === value.layout) < 0) {
+            if (value.layout) {
+                if (
+                    (isHorizontalLogicLayout(layout) && isVerticalLogicLayout(value.layout)) ||
+                    (isVerticalLogicLayout(layout) && isHorizontalLogicLayout(value.layout))
+                ) {
                     Transforms.setNode(board, { layout: null }, [...path, index]);
                 }
-            }
-            if (value.children?.length) {
-                correctLogicLayoutNode(board, layout, [...path, index]);
+                if (value.children?.length) {
+                    correctLogicLayoutNode(board, layout, [...path, index]);
+                }
             }
         });
     }
