08a2dd2d3cb858074197d1d83e5a8ba8219d1a05
gzy <1056342711@qq.com>
2022-10-19 15:19:13 +0800

    fix(mindmap): use correctLayout #WIK-8942

--- packages/mindmap/src/queries/get-branch-mindmap-layouts-by-element.ts
+++ packages/mindmap/src/queries/get-branch-mindmap-layouts-by-element.ts
@@ -1,11 +1,12 @@
 import { MindmapElement } from '../interfaces';
 import { findParentElement } from '../utils';
 import { MindmapLayoutType } from '@plait/layouts';
+import { getCorrectLayoutByElement } from './get-correct-layout-by-element';
 
 export const getBranchMindmapLayouts = (element: MindmapElement) => {
     const layouts: MindmapLayoutType[] = [];
     if (element.layout) {
-        layouts.unshift(element.layout);
+        layouts.unshift(getCorrectLayoutByElement(element));
     }
     let parent = findParentElement(element);
     while (parent) {
