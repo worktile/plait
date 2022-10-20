67b8c2633fd731a6ade93fcba34ba938d6cd2f40
ajie <m18210870360@163.com>
2022-10-20 10:14:19 +0800

    feat: Mixed layouts in left and right layouts are supported of mindmap dnd #WIK-9017

--- packages/mindmap/src/utils/drop-target-corrector.ts
+++ packages/mindmap/src/utils/drop-target-corrector.ts
@@ -13,59 +13,64 @@ export const readjustmentDropTarget = (dropTarget: {
     const newDropTarget = { target, detectResult };
     const targetComponent = MINDMAP_ELEMENT_TO_COMPONENT.get(target) as MindmapNodeComponent;
     if (targetComponent.node.children.length > 0 && dropTarget.detectResult) {
+        const layout = MindmapQueries.getCorrectLayoutByElement(targetComponent.node.origin);
+        const parentLayout = MindmapQueries.getCorrectLayoutByElement(
+            targetComponent.node.origin.isRoot ? targetComponent.node.origin : targetComponent.node.parent.origin
+        );
+        const isFixedLayout = layout !== parentLayout;
         if (['right', 'left'].includes(dropTarget.detectResult)) {
-            if (targetComponent.node.origin.isRoot) {
-                const layout = MindmapQueries.getCorrectLayoutByElement(targetComponent.node.origin);
-                // 标准布局，根节点
-                if (isStandardLayout(layout)) {
-                    const rightNodeCount = targetComponent.node.origin.rightNodeCount as number;
-                    if (detectResult === 'left') {
-                        // 作为左的第一个节点
-                        if (targetComponent.node.children.length === rightNodeCount) {
+            if (!isFixedLayout) {
+                if (targetComponent.node.origin.isRoot) {
+                    const layout = MindmapQueries.getCorrectLayoutByElement(targetComponent.node.origin);
+                    // 标准布局，根节点
+                    if (isStandardLayout(layout)) {
+                        const rightNodeCount = targetComponent.node.origin.rightNodeCount as number;
+                        if (detectResult === 'left') {
+                            // 作为左的第一个节点
+                            if (targetComponent.node.children.length === rightNodeCount) {
+                                return newDropTarget;
+                            }
+                        } else {
+                            // 作为右的第一个节点或最后一个节点
+                            if (rightNodeCount === 0) {
+                                newDropTarget.target = target;
+                            } else {
+                                newDropTarget.target = targetComponent.node.children[rightNodeCount - 1].origin;
+                                newDropTarget.detectResult = 'bottom';
+                            }
                             return newDropTarget;
                         }
-                    } else {
-                        // 作为右的第一个节点或最后一个节点
-                        if (rightNodeCount === 0) {
-                            newDropTarget.target = target;
-                        } else {
-                            newDropTarget.target = targetComponent.node.children[rightNodeCount - 1].origin;
-                            newDropTarget.detectResult = 'bottom';
-                        }
+                    }
+                }
+
+                // 缩进布局探测到第一个子节点
+                if (isIndentedLayout(parentLayout)) {
+                    newDropTarget.target = targetComponent.node.children[0].origin;
+                    newDropTarget.detectResult = isTopLayout(parentLayout) ? 'bottom' : 'top';
+                    return newDropTarget;
+                }
+                // 上下布局的根节点只可以探测到上或者下，子节点的左右探测不处理，跳过。
+                if (isVerticalLogicLayout(parentLayout)) {
+                    return newDropTarget;
+                }
+                // 剩下是水平布局的默认情况：插入最后一个子节点的下方
+                const lastChildNodeIndex = targetComponent.node.children.length - 1;
+                newDropTarget.target = targetComponent.node.children[lastChildNodeIndex].origin;
+                newDropTarget.detectResult = 'bottom';
+            } else {
+                // 处理左右布局下的混合布局
+                if ([MindmapLayoutType.left, MindmapLayoutType.right].includes(parentLayout)) {
+                    const layout = MindmapQueries.getCorrectLayoutByElement(targetComponent.node.origin);
+                    if (isIndentedLayout(layout)) {
+                        newDropTarget.target = targetComponent.node.children[0].origin;
+                        newDropTarget.detectResult = isTopLayout(layout) ? 'bottom' : 'top';
                         return newDropTarget;
                     }
                 }
             }
-            const parentLayout = MindmapQueries.getCorrectLayoutByElement(
-                targetComponent.node.origin.isRoot ? targetComponent.node.origin : targetComponent.node.parent.origin
-            );
-            // 缩进布局探测到第一个子节点
-            if (isIndentedLayout(parentLayout)) {
-                newDropTarget.target = targetComponent.node.children[0].origin;
-                newDropTarget.detectResult = isTopLayout(parentLayout) ? 'bottom' : 'top';
-                return newDropTarget;
-            }
-            // 上下布局的根节点只可以探测到上或者下，子节点的左右探测不处理，跳过。
-            if (isVerticalLogicLayout(parentLayout)) {
-                return newDropTarget;
-            }
-            // 处理左右布局下的混合布局
-            if ([MindmapLayoutType.left, MindmapLayoutType.right].includes(parentLayout)) {
-                const layout = MindmapQueries.getCorrectLayoutByElement(targetComponent.node.origin);
-                if (isIndentedLayout(layout)) {
-                    newDropTarget.target = targetComponent.node.children[0].origin;
-                    newDropTarget.detectResult = isTopLayout(layout) ? 'bottom' : 'top';
-                    return newDropTarget;
-                }
-            }
-            // 剩下是水平布局的默认情况：插入最后一个子节点的下方
-            const lastChildNodeIndex = targetComponent.node.children.length - 1;
-            newDropTarget.target = targetComponent.node.children[lastChildNodeIndex].origin;
-            newDropTarget.detectResult = 'bottom';
         }
         if (['top', 'bottom'].includes(dropTarget.detectResult)) {
             // 缩进布局移动至第一个节点
-            const layout = MindmapQueries.getCorrectLayoutByElement(targetComponent.node.origin);
             if (targetComponent.node.origin.isRoot && isIndentedLayout(layout)) {
                 newDropTarget.target = targetComponent.node.children[0].origin;
                 newDropTarget.detectResult = isTopLayout(layout) ? 'bottom' : 'top';
