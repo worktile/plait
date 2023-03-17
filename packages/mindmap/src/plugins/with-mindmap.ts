4ae3b3254279a14c3d9875cdb8c513a5e5845fd3
WBbug <1056342711@qq.com>
2023-03-17 11:51:03 +0800

    feat(mindmap): add clipboard

--- packages/mindmap/src/plugins/with-mindmap.ts
+++ packages/mindmap/src/plugins/with-mindmap.ts
@@ -1,6 +1,5 @@
 import {
     addSelectedElement,
-    CLIP_BOARD_FORMAT_KEY,
     ELEMENT_TO_PLUGIN_COMPONENT,
     getSelectedElements,
     hotkeys,
@@ -23,12 +22,12 @@ import { getWidthByText } from '@plait/richtext';
 import { MindmapNodeElement, PlaitMindmap } from '../interfaces';
 import { MindmapNode } from '../interfaces/node';
 import { PlaitMindmapComponent } from '../mindmap.component';
-import { buildMindmap, buildNodes, changeRightNodeCount, createEmptyNode, extractNodesText, filterChildElement, findPath } from '../utils';
-import { getRectangleByNode, getRectangleByNodes, hitMindmapNode } from '../utils/graph';
+import { changeRightNodeCount, createEmptyNode, filterChildElement, findPath } from '../utils';
+import { getRectangleByNode, hitMindmapNode } from '../utils/graph';
 import { isVirtualKey } from '../utils/is-virtual-key';
 import { MINDMAP_ELEMENT_TO_COMPONENT } from '../utils/weak-maps';
 import { withNodeDnd } from './with-dnd';
-import { MindmapNodeComponent } from '../node.component';
+import { buildClipBoardData, getDataFromClipBoard, insertClipBoardData, setClipBoardData } from '../utils/clipboard';
 
 export const withMindmap: PlaitPlugin = (board: PlaitBoard) => {
     const { drawElement, dblclick, keydown, insertFragment, setFragment, deleteFragment, isIntersectionSelection } = board;
@@ -229,68 +228,3 @@ export const withMindmap: PlaitPlugin = (board: PlaitBoard) => {
 
     return withNodeDnd(board);
 };
-
-export function buildClipBoardData(selectedNodes: PlaitElement[]) {
-    let result: PlaitElement[] = [];
-    const selectedMindmapNodes = Array.from(selectedNodes, node => {
-        return (MINDMAP_ELEMENT_TO_COMPONENT.get(node as MindmapNodeElement) as MindmapNodeComponent)?.node;
-    });
-
-    const nodesRectangle = getRectangleByNodes(selectedMindmapNodes);
-
-    selectedNodes.forEach((node, index) => {
-        const nodeRectangle = getRectangleByNode(selectedMindmapNodes[index]);
-        result.push({
-            ...node,
-            points: [[nodeRectangle.x - nodesRectangle.x, nodeRectangle.y - nodesRectangle.y]]
-        });
-    });
-
-    return result;
-}
-
-export function setClipBoardData(data: DataTransfer | null, nodeData: PlaitElement[]) {
-    const stringObj = JSON.stringify(nodeData);
-    const encoded = window.btoa(encodeURIComponent(stringObj));
-    const text = nodeData.reduce((string, currentNode) => {
-        return string + extractNodesText(currentNode.nodeData as MindmapNodeElement);
-    }, '');
-    data?.setData(`application/${CLIP_BOARD_FORMAT_KEY}`, encoded);
-    data?.setData(`text/plain`, text);
-}
-
-export function getDataFromClipBoard(data: DataTransfer | null) {
-    const encoded = data?.getData(`application/${CLIP_BOARD_FORMAT_KEY}`);
-    let nodesData: PlaitElement[] = [];
-    if (encoded) {
-        const decoded = decodeURIComponent(window.atob(encoded));
-        nodesData = JSON.parse(decoded);
-    }
-    return nodesData;
-}
-
-export function insertClipBoardData(board: PlaitBoard, nodesData: PlaitElement[], targetPoint: Point) {
-    let selectedElementPath: Path, newElement: MindmapNodeElement, path: Path;
-    const element = getSelectedElements(board)?.[0];
-    const selectedComponent = MINDMAP_ELEMENT_TO_COMPONENT.get(element as MindmapNodeElement) as MindmapNodeComponent;
-
-    if (selectedComponent) {
-        selectedElementPath = findPath(board, selectedComponent.node);
-    }
-
-    nodesData.forEach((item: PlaitElement, index: number) => {
-        if (getSelectedElements(board).length === 1) {
-            newElement = buildNodes(item as MindmapNodeElement);
-            path = selectedElementPath.concat(selectedComponent.node.children.length + index);
-        } else {
-            newElement = buildMindmap(item as MindmapNodeElement, [
-                targetPoint[0] + item.points![0][0],
-                targetPoint[1] + item.points![0][1]
-            ]);
-            path = [board.children.length];
-        }
-
-        Transforms.insertNode(board, newElement, path);
-        return;
-    });
-}
