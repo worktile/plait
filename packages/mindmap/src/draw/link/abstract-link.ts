ea8035ed2dd3b9ad30b8febb1b82767f62f35ec4
pubuzhixing8 <pubuzhixing@gmail.com>
2023-04-20 16:57:12 +0800

    chore: format

--- packages/mindmap/src/draw/link/abstract-link.ts
+++ packages/mindmap/src/draw/link/abstract-link.ts
@@ -13,27 +13,33 @@ export function drawAbstractLink(board: PlaitBoard, node: MindmapNode, isHorizon
         return node.origin;
     });
     const includedElementsRectangle = getRectangleByElements(board, includedElements, true);
+
     const linkDirection = getLinkDirection(node, isHorizontal);
     const bezierBeginPlacement = [HorizontalPlacement.right, VerticalPlacement.top] as PointPlacement;
     const bezierEndPlacement = [HorizontalPlacement.right, VerticalPlacement.bottom] as PointPlacement;
     const abstractConnectorPlacement = [HorizontalPlacement.left, VerticalPlacement.middle] as PointPlacement;
+
     transformPlacement(bezierBeginPlacement, linkDirection);
     transformPlacement(bezierEndPlacement, linkDirection);
     transformPlacement(abstractConnectorPlacement, linkDirection);
+
     let bezierBeginPoint = getPointByPlacement(includedElementsRectangle, bezierBeginPlacement);
     let bezierEndPoint = getPointByPlacement(includedElementsRectangle, bezierEndPlacement);
     let abstractConnectorPoint = getPointByPlacement(abstractRectangle, abstractConnectorPlacement);
+
     let curveDistance = 0
     if (isHorizontal) {
         curveDistance = Math.abs(abstractConnectorPoint[0] - bezierBeginPoint[0]) - linkPadding * 2;
     } else {
         curveDistance = Math.abs(abstractConnectorPoint[1] - bezierBeginPoint[1]) - linkPadding * 2;
     }
+
     bezierBeginPoint = movePoint(bezierBeginPoint, linkPadding, linkDirection);
     let c1 = movePoint(bezierBeginPoint, curveDistance, linkDirection);
     bezierEndPoint = movePoint(bezierEndPoint, linkPadding, linkDirection);
     let c2 = movePoint(bezierEndPoint, curveDistance, linkDirection);
     let bezierConnectorPoint = movePoint(abstractConnectorPoint, -linkPadding, linkDirection);
+    
     const link = PlaitBoard.getRoughSVG(board).path(
         `M${bezierBeginPoint[0]},${bezierBeginPoint[1]} Q${c1[0]},${c1[1]} ${bezierConnectorPoint[0]},${bezierConnectorPoint[1]} Q${c2[0]},${c2[1]} ${bezierEndPoint[0]},${bezierEndPoint[1]} M${abstractConnectorPoint[0]},${abstractConnectorPoint[1]} L${bezierConnectorPoint[0]},${bezierConnectorPoint[1]}`,
         {
