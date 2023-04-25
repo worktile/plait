ef05530ebfe705589fbd8b853efac169ac64df59
WBbug <1056342711@qq.com>
2023-04-25 10:05:11 +0800

    feat(layout): logic better code

--- packages/layouts/src/utils/abstract.ts
+++ packages/layouts/src/utils/abstract.ts
@@ -6,3 +6,19 @@ export const getChildrenSkipAbstract = (treeNode: LayoutTreeNode) => {
         return !AbstractNode.isAbstract(child.origin.origin);
     });
 };
+
+export const getAbstractNodeByEndNode = (nodeParent: LayoutTreeNode, endNode: LayoutTreeNode) => {
+    const nodeIndex = nodeParent.children.indexOf(endNode);
+    return nodeParent.children.find(child => {
+        const abstractNode = child.origin.origin as AbstractNode;
+        return abstractNode.end === nodeIndex;
+    });
+};
+
+export const getAbstractNodeByStartNode = (nodeParent: LayoutTreeNode, startNode: LayoutTreeNode) => {
+    const nodeIndex = nodeParent.children.indexOf(startNode);
+    return nodeParent.children.find(child => {
+        const abstractNode = child.origin.origin as AbstractNode;
+        return abstractNode.start === nodeIndex;
+    });
+};
