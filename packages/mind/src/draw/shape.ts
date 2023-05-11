c4f220ddf26364fb87e961f87603fbe68ef77fd3
pubuzhixing8 <pubuzhixing@gmail.com>
2023-05-11 10:30:21 +0800

    feat(mind): add replaceEmoji method and fix addEmoji error rename mind map to mind

--- packages/mind/src/draw/shape.ts
+++ packages/mind/src/draw/shape.ts
@@ -1,7 +1,6 @@
-import { RoughSVG } from 'roughjs/bin/svg';
-import { NODE_FILL, ROOT_NODE_FILL, STROKE_WIDTH, TRANSPARENT } from '../constants';
+import { NODE_FILL, ROOT_NODE_FILL, STROKE_WIDTH } from '../constants';
 import { MindNode } from '../interfaces/node';
-import { getStrokeByMindmapElement } from '../utils/colors';
+import { getStrokeByMindElement } from '../utils/colors';
 import { getRectangleByNode } from '../utils/graph';
 import { PlaitBoard, drawRoundRectangle } from '@plait/core';
 
@@ -9,7 +8,7 @@ export function drawRectangleNode(board: PlaitBoard, node: MindNode) {
     const { x, y, width, height } = getRectangleByNode(node);
 
     const fill = node.origin.fill ? node.origin.fill : node.origin.isRoot ? ROOT_NODE_FILL : NODE_FILL;
-    const stroke = getStrokeByMindmapElement(node.origin);
+    const stroke = getStrokeByMindElement(node.origin);
     const strokeWidth = node.origin.strokeWidth ? node.origin.strokeWidth : STROKE_WIDTH;
 
     const nodeG = drawRoundRectangle(PlaitBoard.getRoughSVG(board), x, y, x + width, y + height, {
