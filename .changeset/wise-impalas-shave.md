e615a8b7ee5a81aa8e465b5cf6acf62f52b5c7d2
pubuzhixing8 <pubuzhixing@gmail.com>
2023-05-16 11:41:15 +0800

    feat(mind): handle overall abstract when copy node and paste

---
+++ .changeset/wise-impalas-shave.md
@@ -0,0 +1,6 @@
+---
+'@plait/layouts': patch
+'@plait/mind': patch
+---
+
+handle overall abstract when copy node and paste
--- packages/mind/src/utils/clipboard.ts
+++ packages/mind/src/utils/clipboard.ts
@@ -11,20 +11,58 @@ import {
 import { MindElement } from '../interfaces';
 import { copyNewNode, extractNodesText, transformNodeToRoot, transformRootToNode, createMindElement, transformAbstractToNode } from '.';
 import { getRectangleByNode } from './graph';
-import { AbstractNode } from '@plait/layouts';
+import { AbstractNode, getNonAbstractChildren } from '@plait/layouts';
+import { getOverallAbstracts } from './abstract/common';
+import { findNewChildNodePath } from './path';
 
 export const buildClipboardData = (board: PlaitBoard, selectedElements: MindElement[]) => {
     let result: MindElement[] = [];
-    const selectedMindNodes = selectedElements.map(value => MindElement.getNode(value));
-    const nodesRectangle = getRectangleByElements(board, selectedElements, true);
-    selectedElements.forEach((node, index) => {
-        const nodeRectangle = getRectangleByNode(selectedMindNodes[index]);
-        result.push({
-            ...node,
-            points: [[nodeRectangle.x - nodesRectangle.x, nodeRectangle.y - nodesRectangle.y]]
-        });
-    });
 
+    // get overall abstract
+    const overallAbstracts = getOverallAbstracts(board, selectedElements) as MindElement[];
+
+    // keep correct order
+    const newSelectedElements = selectedElements.filter((value) => !overallAbstracts.includes(value));
+    newSelectedElements.push(...overallAbstracts);
+
+    // get correct start and end in selected elements
+    function getCorrectStartEnd(abstract: MindElement & AbstractNode) {
+        const parent = MindElement.getParent(abstract);
+        const startElement = parent.children[abstract.start];
+        const index = selectedElements.indexOf(startElement);
+        return { start: index, end: index + (abstract.end - abstract.start) };
+    }
+
+    const selectedMindNodes = newSelectedElements.map(value => MindElement.getNode(value));
+    const nodesRectangle = getRectangleByElements(board, newSelectedElements, true);
+    newSelectedElements.forEach((element, index) => {
+        // handle relative location
+        const nodeRectangle = getRectangleByNode(selectedMindNodes[index]);
+        const points = [[nodeRectangle.x - nodesRectangle.x, nodeRectangle.y - nodesRectangle.y]] as Point[];
+
+        // handle invalid abstract
+        if (AbstractNode.isAbstract(element) && overallAbstracts.includes(element)) {
+            const { start, end } = getCorrectStartEnd(element);
+            result.push({
+                ...element,
+                points,
+                start,
+                end
+            });
+        } else {
+            if (AbstractNode.isAbstract(element)) {
+                let newElement = { ...element, points } as MindElement;
+                delete newElement.start;
+                delete newElement.end;
+                result.push(newElement);
+            } else {
+                result.push({
+                    ...element,
+                    points: points
+                });
+            }
+        }
+    });
     return result;
 };
 
@@ -53,18 +91,24 @@ export const insertClipboardData = (board: PlaitBoard, elements: PlaitElement[],
     const selectedElements = getSelectedElements(board);
     let newELements: PlaitElement[] = [];
 
+    const hasTargetParent = selectedElements.length === 1;
+    const targetParent = selectedElements[0];
+    const targetParentPath = targetParent && PlaitBoard.findPath(board, targetParent);
+    const nonAbstractChildrenLength = targetParent && getNonAbstractChildren(targetParent).length;
+
     elements.forEach((item: PlaitElement, index: number) => {
         newElement = copyNewNode(item as MindElement);
 
-        if (selectedElements.length === 1) {
+        if (hasTargetParent) {
             if (item.isRoot) {
                 newElement = transformRootToNode(board, newElement);
             }
-            if (AbstractNode.isAbstract(item)) {
-                newElement = transformAbstractToNode(newElement);
+            // handle abstract start and end
+            if (AbstractNode.isAbstract(newElement)) {
+                newElement.start = newElement.start + nonAbstractChildrenLength;
+                newElement.end = newElement.end + nonAbstractChildrenLength;
             }
-            const selectedElementPath = PlaitBoard.findPath(board, selectedElements[0]);
-            path = selectedElementPath.concat((selectedElements[0].children || []).length + index);
+            path = [...targetParentPath, nonAbstractChildrenLength + index];
         } else {
             const point: Point = [targetPoint[0] + item.points![0][0], targetPoint[1] + item.points![0][1]];
             newElement.points = [point];
