130b8d9a8e3163473368c92493c22fd8ae71261e
WBbug <1056342711@qq.com>
2023-06-08 11:53:18 +0800

    test(mind): add branch test (#410)

---
+++ packages/mind/src/utils/node-style/branch.spec.ts
@@ -0,0 +1,31 @@
+import { PlaitBoard, ThemeColorMode, clearNodeWeakMap, createTestingBoard, fakeNodeWeakMap } from '@plait/core';
+import { getTestingChildren } from '../../testing/data/basic';
+import { getDefaultBranchColorByIndex } from './branch';
+import { COLORFUL_BRANCH_COLORS, DEFAULT_BRANCH_COLORS } from '../../constants/theme';
+import { MindThemeColor } from '../../interfaces/theme-color';
+
+describe('utils node-style branch', () => {
+    let board: PlaitBoard;
+    beforeEach(() => {
+        const children = getTestingChildren();
+        board = createTestingBoard([], children, { themeColors: MindThemeColor });
+        fakeNodeWeakMap(board);
+    });
+
+    afterEach(() => {
+        clearNodeWeakMap(board);
+    });
+
+    it('get default theme branch color by index', () => {
+        const index = 3;
+        const color = getDefaultBranchColorByIndex(board, index);
+        expect(color).toEqual(DEFAULT_BRANCH_COLORS[index]);
+    });
+
+    it('get colorful theme branch color by index', () => {
+        const index = 3;
+        board.theme.themeColorMode = ThemeColorMode.colorful;
+        const color = getDefaultBranchColorByIndex(board, index);
+        expect(color).toEqual(COLORFUL_BRANCH_COLORS[index]);
+    });
+});
