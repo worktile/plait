370c3345a89c7d85f8703f1171acbd8c7375631e
WBbug <1056342711@qq.com>
2023-06-08 11:51:03 +0800

    feat(mind): change index location

--- packages/mind/src/utils/node-style/branch.spec.ts
+++ packages/mind/src/utils/node-style/branch.spec.ts
@@ -6,7 +6,6 @@ import { MindThemeColor } from '../../interfaces/theme-color';
 
 describe('utils node-style branch', () => {
     let board: PlaitBoard;
-    const index = 3;
     beforeEach(() => {
         const children = getTestingChildren();
         board = createTestingBoard([], children, { themeColors: MindThemeColor });
@@ -18,11 +17,13 @@ describe('utils node-style branch', () => {
     });
 
     it('get default theme branch color by index', () => {
+        const index = 3;
         const color = getDefaultBranchColorByIndex(board, index);
         expect(color).toEqual(DEFAULT_BRANCH_COLORS[index]);
     });
 
     it('get colorful theme branch color by index', () => {
+        const index = 3;
         board.theme.themeColorMode = ThemeColorMode.colorful;
         const color = getDefaultBranchColorByIndex(board, index);
         expect(color).toEqual(COLORFUL_BRANCH_COLORS[index]);
