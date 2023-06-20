90964849bea69c1b20bb64f0810193b4347c6a87
WBbug <1056342711@qq.com>
2023-06-20 11:14:46 +0800

    feat(mind): add CollapseDrawer

--- packages/mind/src/constants/default.ts
+++ packages/mind/src/constants/default.ts
@@ -7,7 +7,7 @@ export const STROKE_WIDTH = 3;
 export const BRANCH_WIDTH = 3;
 
 export const EXTEND_OFFSET = 8;
-export const EXTEND_RADIUS = 16;
+export const EXTEND_DIAMETER = 16;
 
 export const QUICK_INSERT_CIRCLE_OFFSET = 9;
 export const QUICK_INSERT_CIRCLE_COLOR = '#6698FF';
