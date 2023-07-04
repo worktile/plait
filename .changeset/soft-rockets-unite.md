21393a78223e5c06d1c0462f7246abbbc907b0d0
pubuzhixing8 <pubuzhixing@gmail.com>
2023-07-04 10:46:05 +0800

    fix(text): correct the value of CLIPBOARD_FORMAT_KEY in plait text

---
+++ .changeset/soft-rockets-unite.md
@@ -0,0 +1,5 @@
+---
+'@plait/text': patch
+---
+
+correct the value of CLIPBOARD_FORMAT_KEY in plait text
--- packages/text/src/constant/index.ts
+++ packages/text/src/constant/index.ts
@@ -2,4 +2,4 @@ export * from './mark';
 
 export const TEXT_DEFAULT_HEIGHT = 20;
 
-export const CLIPBOARD_FORMAT_KEY = 'x-plait-fragment';
+export const CLIPBOARD_FORMAT_KEY = 'x-plait-text-fragment';
