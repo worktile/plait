e731e703c29910734c4f80ee138eb9c9a3f960c4
pubuzhixing8 <pubuzhixing@gmail.com>
2023-07-06 15:14:37 +0800

    feat(core): add PlaitIslandPopoverBaseComponent to support popover case

--- packages/plait/src/core/island/island-base.component.ts
+++ packages/plait/src/core/island/island-base.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Directive } from '@angular/core';
+import { ChangeDetectorRef, Directive, Input, OnDestroy, OnInit } from '@angular/core';
 import { PlaitBoard } from '../../interfaces';
 
 @Directive({
@@ -19,6 +19,52 @@ export abstract class PlaitIslandBaseComponent {
     markForCheck() {
         this.cdr.markForCheck();
     }
+}
+
+@Directive({
+    host: {
+        class: 'plait-island-popover-container'
+    }
+})
+export abstract class PlaitIslandPopoverBaseComponent implements OnInit, OnDestroy {
+    @Input() board!: PlaitBoard;
+
+    private onChange!: () => void;
+
+    initialize(board: PlaitBoard) {
+        this.board = board;
+        const onChange = board.onChange;
+        board.onChange = () => {
+            onChange();
+            if (hasOnBoardChange(this)) {
+                this.onBoardChange();
+            }
+        };
+        this.onChange = onChange;
+    }
+
+    ngOnInit(): void {
+        if (!this.board) {
+            throw new Error('can not find board instance');
+        }
+        this.initialize(this.board);
+        this.islandOnInit();
+    }
+
+    ngOnDestroy(): void {
+        this.board.onChange = this.onChange;
+        this.islandOnDestroy();
+    }
+
+    /**
+     * use islandOnInit replace ngOnInit to avoid ngOnInit being overridden
+     */
+    abstract islandOnInit(): void;
+
+    /**
+     * use islandOnDestroy replace ngOnDestroy to avoid ngOnDestroy being overridden
+     */
+    abstract islandOnDestroy(): void;
 }
 
 export interface OnBoardChange {
