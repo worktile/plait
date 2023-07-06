a5d3b246aee2c10842479c170de663343ec99e11
WBbug <1056342711@qq.com>
2023-07-06 16:05:30 +0800

    feat(text): delete at (#459)

--- packages/text/src/plugins/link/link-editor.ts
+++ packages/text/src/plugins/link/link-editor.ts
@@ -17,7 +17,7 @@ export const LinkEditor = {
             children: isCollapsed ? [{ text }] : []
         };
         if (isCollapsed) {
-            Transforms.insertNodes(editor, link, { at: selection });
+            Transforms.insertNodes(editor, link);
         } else {
             Transforms.wrapNodes(editor, link, { split: true });
             Transforms.collapse(editor, { edge: 'end' });
