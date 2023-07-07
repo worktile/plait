3520d09b386c81825d97ff9bc02ae2748c7d8bf1
WBbug <1056342711@qq.com>
2023-07-07 15:51:57 +0800

    feat(core): add readonly

--- packages/plait/src/plugins/with-hotkey.ts
+++ packages/plait/src/plugins/with-hotkey.ts
@@ -7,47 +7,45 @@ export const withHotkey = (board: PlaitBoard) => {
     const { keydown } = board;
 
     board.keydown = (event: KeyboardEvent) => {
-        if (!PlaitBoard.isReadonly(board)) {
-            if (PlaitBoard.getMovingPoint(board)) {
-                if (isHotkey(['mod+=', 'mod++'], { byKey: true })(event)) {
-                    event.preventDefault();
-                    BoardTransforms.updateZoom(board, board.viewport.zoom + 0.1, false);
-                }
-                if (isHotkey('mod+-', { byKey: true })(event)) {
-                    event.preventDefault();
-                    BoardTransforms.updateZoom(board, board.viewport.zoom - 0.1);
-                }
-                if (isHotkey('mod+0', { byKey: true })(event)) {
-                    event.preventDefault();
-                    BoardTransforms.updateZoom(board, 1);
-                    return;
-                }
-                if (isHotkey('mod+shift+=', { byKey: true })(event)) {
-                    event.preventDefault();
-                    BoardTransforms.fitViewport(board);
-                    return;
-                }
-            }
+        if (!PlaitBoard.isReadonly(board) && isHotkey('mod+a', event)) {
+            event.preventDefault();
+            let elements: PlaitElement[] = [];
+            depthFirstRecursion<Ancestor>(
+                board,
+                node => {
+                    elements.push(node as PlaitElement);
+                },
+                node => {
+                    if (PlaitBoard.isBoard(node) || board.isRecursion(node)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                },
+                true
+            );
 
-            if (isHotkey('mod+a', event)) {
+            Transforms.setSelectionWithTemporaryElements(board, elements);
+        }
+
+        if (PlaitBoard.getMovingPoint(board)) {
+            if (isHotkey(['mod+=', 'mod++'], { byKey: true })(event)) {
                 event.preventDefault();
-                let elements: PlaitElement[] = [];
-                depthFirstRecursion<Ancestor>(
-                    board,
-                    node => {
-                        elements.push(node as PlaitElement);
-                    },
-                    node => {
-                        if (PlaitBoard.isBoard(node) || board.isRecursion(node)) {
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    },
-                    true
-                );
-
-                Transforms.setSelectionWithTemporaryElements(board, elements);
+                BoardTransforms.updateZoom(board, board.viewport.zoom + 0.1, false);
+            }
+            if (isHotkey('mod+-', { byKey: true })(event)) {
+                event.preventDefault();
+                BoardTransforms.updateZoom(board, board.viewport.zoom - 0.1);
+            }
+            if (isHotkey('mod+0', { byKey: true })(event)) {
+                event.preventDefault();
+                BoardTransforms.updateZoom(board, 1);
+                return;
+            }
+            if (isHotkey('mod+shift+=', { byKey: true })(event)) {
+                event.preventDefault();
+                BoardTransforms.fitViewport(board);
+                return;
             }
         }
 
