47c426dcd0561aa6614c5220c35d6a986b2f216d
WBbug <1056342711@qq.com>
2023-07-10 10:44:07 +0800

    feat(text): handle paste link

--- packages/text/src/plugins/link/with-link.ts
+++ packages/text/src/plugins/link/with-link.ts
@@ -1,9 +1,11 @@
 import { Element, NodeEntry, Transforms, Node } from 'slate';
 import { LinkElement } from '../../custom-types';
 import { AngularEditor } from 'slate-angular';
+import { CLIPBOARD_FORMAT_KEY } from '../../constant';
+import { LinkEditor } from './link-editor';
 
 export const withLink = <T extends AngularEditor>(editor: T): T => {
-    const { isInline, normalizeNode } = editor;
+    const { isInline, normalizeNode, insertData } = editor;
 
     editor.isInline = (element: Element) => {
         return (element as LinkElement).type === 'link' ? true : isInline(element);
@@ -20,5 +22,43 @@ export const withLink = <T extends AngularEditor>(editor: T): T => {
         normalizeNode(nodeEntry);
     };
 
+    editor.insertData = data => {
+        const text = data.getData('text/plain');
+        const fragment = data.getData(`application/${CLIPBOARD_FORMAT_KEY}`);
+
+        if (text && isUrl(text) && !fragment) {
+            LinkEditor.wrapLink(editor, text, text);
+            Transforms.move(editor, { distance: 1, unit: 'offset' });
+        } else {
+            insertData(data);
+        }
+    };
+
     return editor;
 };
+
+function isUrl(string: string) {
+    const protocolAndDomainRE = /^(?:\w+:)?\/\/(\S+)$/;
+    const localhostDomainRE = /^localhost[\:?\d]*(?:[^\:?\d]\S*)?$/;
+    const nonLocalhostDomainRE = /^[^\s\.]+\.\S{2,}$/;
+
+    if (typeof string !== 'string') {
+        return false;
+    }
+
+    var match = string.match(protocolAndDomainRE);
+    if (!match) {
+        return false;
+    }
+
+    var everythingAfterProtocol = match[1];
+    if (!everythingAfterProtocol) {
+        return false;
+    }
+
+    if (localhostDomainRE.test(everythingAfterProtocol) || nonLocalhostDomainRE.test(everythingAfterProtocol)) {
+        return true;
+    }
+
+    return false;
+}
