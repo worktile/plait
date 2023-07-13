7fc45994a7d01ad6a96de0f1ac5da8237e378d64
WBbug <1056342711@qq.com>
2023-07-13 17:30:43 +0800

    feat(text): add board on editor

---
+++ .changeset/hungry-rivers-serve.md
@@ -0,0 +1,5 @@
+---
+'@plait/text': minor
+---
+
+add board on editor
--- packages/text/src/plugins/text.editor.ts
+++ packages/text/src/plugins/text.editor.ts
@@ -1,6 +1,8 @@
+import { PlaitBoard } from '@plait/core';
 import { Element } from 'slate';
 import { AngularEditor, ViewType } from 'slate-angular';
 
 export interface PlaitTextEditor extends AngularEditor {
     renderElement?: (element: Element) => ViewType;
+    board?: PlaitBoard;
 }
