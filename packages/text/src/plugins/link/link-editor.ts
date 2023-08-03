efaae4adc07d66c064c9f9040a72c210d95045fa
WBbug <1056342711@qq.com>
2023-08-03 15:19:26 +0800

    fix(text): add readonly judge and getLinkElement function, delete  setSelection #WIK-12880 (#503)

--- packages/text/src/plugins/link/link-editor.ts
+++ packages/text/src/plugins/link/link-editor.ts
@@ -1,11 +1,9 @@
 import { Editor, Transforms, Range, Element, BaseRange, Location } from 'slate';
 import { AngularEditor } from 'slate-angular';
 import { CustomElement, LinkElement } from '../../custom-types';
-import { setSelection } from '../mark/mark.editor';
 
 export const LinkEditor = {
     wrapLink(editor: AngularEditor, text: string, url: string) {
-        setSelection(editor);
         if (LinkEditor.isLinkActive(editor)) {
             LinkEditor.unwrapLink(editor);
         }
@@ -14,9 +12,13 @@ export const LinkEditor = {
         const link: LinkElement = {
             type: 'link',
             url,
-            children: isCollapsed ? [{ text }] : []
+            children: [{ text }]
         };
-        if (isCollapsed) {
+
+        if (AngularEditor.isReadonly(editor)) {
+            const at = { anchor: Editor.start(editor, [0]), focus: Editor.end(editor, [0]) };
+            Transforms.wrapNodes(editor, link, { split: true, at });
+        } else if (isCollapsed) {
             Transforms.insertNodes(editor, link);
         } else {
             Transforms.wrapNodes(editor, link, { split: true });
@@ -24,16 +26,30 @@ export const LinkEditor = {
         }
     },
     unwrapLink(editor: AngularEditor, at?: Location) {
-        !at && setSelection(editor);
+        if (!at) {
+            at = editor.selection as BaseRange;
+            if (AngularEditor.isReadonly(editor) && editor.children && editor.children.length > 0) {
+                at = { anchor: Editor.start(editor, [0]), focus: Editor.end(editor, [0]) };
+            }
+        }
+
         Transforms.unwrapNodes<CustomElement>(editor, { at, match: n => Element.isElement(n) && (n as LinkElement).type === 'link' });
     },
     isLinkActive(editor: AngularEditor) {
         let at = editor.selection as BaseRange;
-        if (!editor.selection && editor.children && editor.children.length > 0) {
+        if (AngularEditor.isReadonly(editor) && editor.children && editor.children.length > 0) {
             at = { anchor: Editor.start(editor, [0]), focus: Editor.end(editor, [0]) };
         }
         const [link] = Editor.nodes<CustomElement>(editor, { match: n => Element.isElement(n) && (n as LinkElement).type === 'link', at });
 
         return !!link;
+    },
+    getLinkElement(editor: AngularEditor) {
+        let at = editor.selection as BaseRange;
+        if (AngularEditor.isReadonly(editor) && editor.children && editor.children.length > 0) {
+            at = { anchor: Editor.start(editor, [0]), focus: Editor.end(editor, [0]) };
+        }
+        const [link] = Editor.nodes<CustomElement>(editor, { match: n => Element.isElement(n) && (n as LinkElement).type === 'link', at });
+        return link;
     }
 };
