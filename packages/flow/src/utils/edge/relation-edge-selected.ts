73ce01f126acb1a6df5457b4db7be13b0570e102
xinglu01 <15245457817@163.com>
2023-08-17 15:39:41 +0800

    feat(flow): support selected node relation edge highlight(#OSP-576)

--- packages/flow/src/utils/edge/relation-edge-selected.ts
+++ packages/flow/src/utils/edge/relation-edge-selected.ts
@@ -1,13 +1,19 @@
-import { PlaitBoard, PlaitElement } from '@plait/core';
+import { PlaitBoard, PlaitElement, getSelectedElements } from '@plait/core';
 import { getEdgesByNodeId } from './get-edges-by-node';
 import { FlowRenderMode } from '../../interfaces/flow';
 import { FlowEdgeComponent } from '../../flow-edge.component';
 
-export const setRelationEdgeSelected = (board: PlaitBoard, nodeId: string, relatedNodeSelected: boolean = false) => {
-    const relationEdges = getEdgesByNodeId(board, nodeId);
-    (relationEdges || []).forEach(edge => {
+export const setRelationEdgeSelected = (board: PlaitBoard, nodeId: string, selected: boolean = false) => {
+    const currentRelationEdges = getEdgesByNodeId(board, nodeId);
+    let selectedRelationEdgeIds: string[] = [];
+    if (!selected) {
+        ((getSelectedElements(board) && getEdgesByNodeId(board, getSelectedElements(board)[0]?.id)) || []).forEach(edge => {
+            selectedRelationEdgeIds.push(edge.id);
+        });
+    }
+    (currentRelationEdges || []).forEach(edge => {
         const component = PlaitElement.getComponent(edge) as FlowEdgeComponent;
-        component.relatedNodeSelected = relatedNodeSelected;
-        component && component.drawElement(edge, relatedNodeSelected ? FlowRenderMode.hover : FlowRenderMode.default);
+        component.relatedNodeSelected = selected || selectedRelationEdgeIds.includes(edge.id);
+        component && component.drawElement(edge, component.relatedNodeSelected ? FlowRenderMode.hover : FlowRenderMode.default);
     });
 };
