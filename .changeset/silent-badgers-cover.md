59e75dbe03ce59592914fd9ad0e2b80c3d2645c5
pubuzhixing8 <pubuzhixing@gmail.com>
2023-08-22 17:00:17 +0800

    feat(common): modify resize handle diameter from 10 to 8

---
+++ .changeset/silent-badgers-cover.md
@@ -0,0 +1,5 @@
+---
+'@plait/common': patch
+---
+
+modify resize handle diameter from 10 to 8
--- packages/common/src/constants/default.ts
+++ packages/common/src/constants/default.ts
@@ -1,3 +1,3 @@
 export const BASE = 4;
 export const PRIMARY_COLOR = '#6698FF';
-export const RESIZE_HANDLE_DIAMETER = 10;
+export const RESIZE_HANDLE_DIAMETER = 8;
