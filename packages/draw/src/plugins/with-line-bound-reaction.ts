67052db87e352fbf691ad2bc0904b22d3f2e1b86
WBbug <1056342711@qq.com>
2023-08-29 16:41:49 +0800

    feat(draw): set nearest point when close to center point (#540)

--- packages/draw/src/plugins/with-line-bound-reaction.ts
+++ packages/draw/src/plugins/with-line-bound-reaction.ts
@@ -8,14 +8,14 @@ import {
     transformPoint
 } from '@plait/core';
 import { PlaitDrawElement, PlaitGeometry } from '../interfaces';
-import { drawBoundMask } from '../utils';
+import { drawBoundMask, getHitEdgeCenterPoint } from '../utils';
 import { DrawPointerType } from '../constants';
 import { getRectangleByPoints, isResizingByCondition } from '@plait/common';
 import { getStrokeWidthByElement } from '../utils/geometry-style/stroke';
 import { getHitOutlineGeometry } from '../utils/position/geometry';
 
 export const withLineBoundReaction = (board: PlaitBoard) => {
-    const { pointerMove } = board;
+    const { pointerMove, pointerUp } = board;
 
     let boundShapeG: SVGGElement | null = null;
 
@@ -33,8 +33,10 @@ export const withLineBoundReaction = (board: PlaitBoard) => {
                 const offset = (getStrokeWidthByElement(board, hitElement) + 1) / 2;
                 const activeRectangle = RectangleClient.getOutlineRectangle(rectangle, -offset);
                 const activeRectangleCornerPoints = RectangleClient.getCornerPoints(activeRectangle);
-                const nearestPoint = getNearestPointBetweenPointAndSegments(movingPoint, activeRectangleCornerPoints);
-                const circleG = drawCircle(PlaitBoard.getRoughSVG(board), nearestPoint, 4, {
+                let nearestPoint = getNearestPointBetweenPointAndSegments(movingPoint, activeRectangleCornerPoints);
+                const activePoint = getHitEdgeCenterPoint(nearestPoint, activeRectangle);
+                nearestPoint = activePoint ? activePoint : nearestPoint;
+                const circleG = drawCircle(PlaitBoard.getRoughSVG(board), nearestPoint, 6, {
                     stroke: SELECTION_BORDER_COLOR,
                     strokeWidth: 1,
                     fill: SELECTION_BORDER_COLOR,
@@ -46,6 +48,13 @@ export const withLineBoundReaction = (board: PlaitBoard) => {
         }
 
         pointerMove(event);
+    };
+
+    board.pointerUp = event => {
+        boundShapeG?.remove();
+        boundShapeG = null;
+
+        pointerUp(event);
     };
 
     return board;
