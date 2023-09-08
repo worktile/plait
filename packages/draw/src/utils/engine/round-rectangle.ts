c42dac031a7a663e00337da26c8ec914597728ca
WBbug <1056342711@qq.com>
2023-09-08 10:53:05 +0800

    feat(draw): change name

---
+++ packages/draw/src/utils/engine/round-rectangle.ts
@@ -0,0 +1,72 @@
+import {
+    PlaitBoard,
+    Point,
+    RectangleClient,
+    drawRoundRectangle,
+    getNearestPointBetweenPointAndSegments,
+    isPointInRoundRectangle
+} from '@plait/core';
+import { ShapeEngine } from '../../interfaces';
+import { Options } from 'roughjs/bin/core';
+import { getNearestPointBetweenPointAndEllipse } from './ellipse';
+
+export const RoundRectangleEngine: ShapeEngine = {
+    draw(board: PlaitBoard, rectangle: RectangleClient, options: Options) {
+        return drawRoundRectangle(
+            PlaitBoard.getRoughSVG(board),
+            rectangle.x,
+            rectangle.y,
+            rectangle.x + rectangle.width,
+            rectangle.y + rectangle.height,
+            options,
+            false,
+            getRoundRectangleRadius(rectangle)
+        );
+    },
+    isHit(rectangle: RectangleClient, point: Point) {
+        return isPointInRoundRectangle(point, rectangle, getRoundRectangleRadius(rectangle));
+    },
+    getNearestPoint(rectangle: RectangleClient, point: Point) {
+        return getNearestPointBetweenPointAndRoundRectangle(point, rectangle, getRoundRectangleRadius(rectangle));
+    },
+    getConnectorPoints(rectangle: RectangleClient) {
+        return RectangleClient.getEdgeCenterPoints(rectangle);
+    }
+};
+
+export const getRoundRectangleRadius = (rectangle: RectangleClient) => {
+    return Math.min(rectangle.width * 0.1, rectangle.height * 0.1);
+};
+
+export function getNearestPointBetweenPointAndRoundRectangle(point: Point, rectangle: RectangleClient, radius: number) {
+    const { x: rectX, y: rectY, width, height } = rectangle;
+    const cornerPoints = RectangleClient.getCornerPoints(rectangle);
+    let result = getNearestPointBetweenPointAndSegments(point, cornerPoints);
+    let circleCenter: Point | null = null;
+
+    const inLeftTop = point[0] >= rectX && point[0] <= rectX + radius && point[1] >= rectY && point[1] <= rectY + radius;
+    if (inLeftTop) {
+        circleCenter = [rectX + radius, rectY + radius];
+    }
+    const inLeftBottom =
+        point[0] >= rectX && point[0] <= rectX + radius && point[1] >= rectY + height && point[1] <= rectY + height - radius;
+    if (inLeftBottom) {
+        circleCenter = [rectX + radius, rectY + height - radius];
+    }
+    const inRightTop = point[0] >= rectX + width - radius && point[0] <= rectX + width && point[1] >= rectY && point[1] <= rectY + radius;
+    if (inRightTop) {
+        circleCenter = [rectX + width - radius, rectY + radius];
+    }
+    const inRightBottom =
+        point[0] >= rectX + width - radius &&
+        point[0] <= rectX + width &&
+        point[1] >= rectY + height - radius &&
+        point[1] <= rectY + height;
+    if (inRightBottom) {
+        circleCenter = [rectX + width - radius, rectY + height - radius];
+    }
+    if (circleCenter) {
+        result = getNearestPointBetweenPointAndEllipse(point, circleCenter, radius, radius);
+    }
+    return result;
+}
--- packages/draw/src/utils/geometry.ts
+++ packages/draw/src/utils/geometry.ts
@@ -6,8 +6,6 @@ import {
     SELECTION_FILL_COLOR,
     createG,
     drawCircle,
-    drawRoundRectangle,
-    getNearestPointBetweenPointAndSegments,
     idCreator
 } from '@plait/core';
 import { GeometryShape, PlaitGeometry } from '../interfaces/geometry';
@@ -17,7 +15,7 @@ import { DefaultTextProperty, ShapeDefaultSpace } from '../constants';
 import { getRectangleByPoints } from '@plait/common';
 import { getStrokeWidthByElement } from './geometry-style/stroke';
 import { Options } from 'roughjs/bin/core';
-import { ShapeMethodsMap } from './shapes';
+import { getEngine } from './engine';
 
 export const createGeometryElement = (
     shape: GeometryShape,
@@ -77,7 +75,7 @@ export const drawBoundMask = (board: PlaitBoard, element: PlaitGeometry) => {
     });
     G.appendChild(maskG);
 
-    const connectorPoints = ShapeMethodsMap[element.shape].getConnectorPoints(activeRectangle);
+    const connectorPoints = getEngine(element.shape).getConnectorPoints(activeRectangle);
     connectorPoints.forEach(point => {
         const circleG = drawCircle(PlaitBoard.getRoughSVG(board), point, 6, {
             stroke: '#999999',
@@ -91,124 +89,15 @@ export const drawBoundMask = (board: PlaitBoard, element: PlaitGeometry) => {
     return G;
 };
 
-export const drawDiamond = (board: PlaitBoard, rectangle: RectangleClient, options: Options) => {
-    const points = RectangleClient.getEdgeCenterPoints(rectangle);
-    const rs = PlaitBoard.getRoughSVG(board);
-    return rs.polygon(points, { ...options, fillStyle: 'solid' });
-};
-
-export const drawParallelogram = (board: PlaitBoard, rectangle: RectangleClient, options: Options) => {
-    const points = getParallelogramPoints(rectangle);
-    const rs = PlaitBoard.getRoughSVG(board);
-    return rs.polygon(points, { ...options, fillStyle: 'solid' });
-};
-
-export const drawEllipse = (board: PlaitBoard, rectangle: RectangleClient, options: Options) => {
-    const centerPoint = [rectangle.x + rectangle.width / 2, rectangle.y + rectangle.height / 2];
-    const rs = PlaitBoard.getRoughSVG(board);
-    return rs.ellipse(centerPoint[0], centerPoint[1], rectangle.width, rectangle.height, { ...options, fillStyle: 'solid' });
-};
-
 export const drawGeometry = (board: PlaitBoard, outerRectangle: RectangleClient, shape: GeometryShape, options: Options) => {
-    return ShapeMethodsMap[shape].draw(board, outerRectangle, options);
-};
-
-export const getRoundRectangleRadius = (rectangle: RectangleClient) => {
-    return Math.min(rectangle.width * 0.1, rectangle.height * 0.1);
-};
-
-export const getParallelogramPoints = (rectangle: RectangleClient): Point[] => {
-    return [
-        [rectangle.x + rectangle.width / 4, rectangle.y],
-        [rectangle.x + rectangle.width, rectangle.y],
-        [rectangle.x + (rectangle.width * 3) / 4, rectangle.y + rectangle.height],
-        [rectangle.x, rectangle.y + rectangle.height]
-    ];
+    return getEngine(shape).draw(board, outerRectangle, options);
 };
 
 export const getNearestPoint = (element: PlaitGeometry, point: Point, offset = 0) => {
     const rectangle = getRectangleByPoints(element.points);
     const activeRectangle = RectangleClient.getOutlineRectangle(rectangle, -offset);
-    return ShapeMethodsMap[element.shape].getNearestPoint(activeRectangle, point);
+    return getEngine(element.shape).getNearestPoint(activeRectangle, point);
 };
-
-export function getNearestPointBetweenPointAndEllipse(point: Point, center: Point, rx: number, ry: number, rotation: number = 0): Point {
-    const rectangleClient = {
-        x: center[0] - rx,
-        y: center[1] - ry,
-        height: ry * 2,
-        width: rx * 2
-    };
-    // https://stackoverflow.com/a/46007540/232122
-    const px = Math.abs(point[0] - rectangleClient.x - rectangleClient.width / 2);
-    const py = Math.abs(point[1] - rectangleClient.y - rectangleClient.height / 2);
-
-    let tx = 0.707;
-    let ty = 0.707;
-
-    const a = Math.abs(rectangleClient.width) / 2;
-    const b = Math.abs(rectangleClient.height) / 2;
-
-    [0, 1, 2, 3].forEach(x => {
-        const xx = a * tx;
-        const yy = b * ty;
-
-        const ex = ((a * a - b * b) * tx ** 3) / a;
-        const ey = ((b * b - a * a) * ty ** 3) / b;
-
-        const rx = xx - ex;
-        const ry = yy - ey;
-
-        const qx = px - ex;
-        const qy = py - ey;
-
-        const r = Math.hypot(ry, rx);
-        const q = Math.hypot(qy, qx);
-
-        tx = Math.min(1, Math.max(0, ((qx * r) / q + ex) / a));
-        ty = Math.min(1, Math.max(0, ((qy * r) / q + ey) / b));
-        const t = Math.hypot(ty, tx);
-        tx /= t;
-        ty /= t;
-    });
-    const signX = point[0] > center[0] ? 1 : -1;
-    const signY = point[1] > center[1] ? 1 : -1;
-
-    return [center[0] + a * tx * signX, center[1] + b * ty * signY];
-}
-
-export function getNearestPointBetweenPointAndRoundRectangle(point: Point, rectangle: RectangleClient, radius: number) {
-    const { x: rectX, y: rectY, width, height } = rectangle;
-    const cornerPoints = RectangleClient.getCornerPoints(rectangle);
-    let result = getNearestPointBetweenPointAndSegments(point, cornerPoints);
-    let circleCenter: Point | null = null;
-
-    const inLeftTop = point[0] >= rectX && point[0] <= rectX + radius && point[1] >= rectY && point[1] <= rectY + radius;
-    if (inLeftTop) {
-        circleCenter = [rectX + radius, rectY + radius];
-    }
-    const inLeftBottom =
-        point[0] >= rectX && point[0] <= rectX + radius && point[1] >= rectY + height && point[1] <= rectY + height - radius;
-    if (inLeftBottom) {
-        circleCenter = [rectX + radius, rectY + height - radius];
-    }
-    const inRightTop = point[0] >= rectX + width - radius && point[0] <= rectX + width && point[1] >= rectY && point[1] <= rectY + radius;
-    if (inRightTop) {
-        circleCenter = [rectX + width - radius, rectY + radius];
-    }
-    const inRightBottom =
-        point[0] >= rectX + width - radius &&
-        point[0] <= rectX + width &&
-        point[1] >= rectY + height - radius &&
-        point[1] <= rectY + height;
-    if (inRightBottom) {
-        circleCenter = [rectX + width - radius, rectY + height - radius];
-    }
-    if (circleCenter) {
-        result = getNearestPointBetweenPointAndEllipse(point, circleCenter, radius, radius);
-    }
-    return result;
-}
 
 export const getCenterPointsOnPolygon = (points: Point[]) => {
     const centerPoint: Point[] = [];
