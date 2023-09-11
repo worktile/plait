2fd88fb1fb937e9c117f91042313edc72218a66c
WBbug <1056342711@qq.com>
2023-09-11 17:57:00 +0800

    fix(draw): cancel draw when is text (#556)

---
+++ .changeset/long-apes-flash.md
@@ -0,0 +1,5 @@
+---
+'@plait/draw': patch
+---
+
+cancel draw when is text
--- packages/draw/src/generator/geometry-shape.generator.ts
+++ packages/draw/src/generator/geometry-shape.generator.ts
@@ -1,4 +1,4 @@
-import { PlaitGeometry } from '../interfaces';
+import { GeometryShape, PlaitGeometry } from '../interfaces';
 import { getRectangleByPoints, Generator } from '@plait/common';
 import { getStrokeColorByElement, getStrokeWidthByElement } from '../utils/geometry-style/stroke';
 import { drawGeometry } from '../utils';
@@ -13,6 +13,9 @@ export class GeometryShapeGenerator extends Generator<PlaitGeometry, ShapeData> 
     baseDraw(element: PlaitGeometry, data: ShapeData) {
         const outerRectangle = getRectangleByPoints(element.points);
         const shape = element.shape;
+        if (shape === GeometryShape.text) {
+            return;
+        }
         const strokeWidth = getStrokeWidthByElement(this.board, element);
         const strokeColor = getStrokeColorByElement(this.board, element);
         this.g = drawGeometry(this.board, outerRectangle, shape, { stroke: strokeColor, strokeWidth });
