1e852767888f7ca1b1b42554d488a42ed3aa08a0
WBbug <1056342711@qq.com>
2023-09-14 17:50:10 +0800

    fix(draw): export draw component (#567)

---
+++ .changeset/wild-cougars-leave.md
@@ -0,0 +1,5 @@
+---
+'@plait/draw': patch
+---
+
+export draw component
--- packages/draw/src/public-api.ts
+++ packages/draw/src/public-api.ts
@@ -2,3 +2,5 @@ export * from './interfaces';
 export * from './plugins/with-draw';
 export * from './constants';
 export * from './utils';
+export * from './geometry.component';
+export * from './line.component';
