9d38324cf882c547949519733f5b0188747646a8
WBbug <1056342711@qq.com>
2023-09-26 15:35:50 +0800

    feat(draw): support fix ratio when resize

--- packages/draw/src/plugins/with-geometry-resize.ts
+++ packages/draw/src/plugins/with-geometry-resize.ts
@@ -1,6 +1,14 @@
 import { PlaitBoard, PlaitElement, Point, getSelectedElements } from '@plait/core';
 import { PlaitGeometry } from '../interfaces/geometry';
-import { ResizeHandle, ResizeRef, ResizeState, WithResizeOptions, normalizeShapePoints, withResize } from '@plait/common';
+import {
+    ResizeHandle,
+    ResizeRef,
+    ResizeState,
+    WithResizeOptions,
+    getRectangleByPoints,
+    normalizeShapePoints,
+    withResize
+} from '@plait/common';
 import { getSelectedGeometryElements } from '../utils/selected';
 import { getHitGeometryResizeHandleRef } from '../utils/position/geometry';
 import { DrawTransforms } from '../transforms';
@@ -48,25 +56,27 @@ export const withGeometryResize = (board: PlaitBoard) => {
         },
         onResize: (resizeRef: ResizeRef<PlaitGeometry>, resizeState: ResizeState) => {
             let points: [Point, Point] = [...resizeRef.element.points];
+            const rectangle = getRectangleByPoints(resizeRef.element.points);
+            const ratio = rectangle.height / rectangle.width;
             if (resizeRef.handle === ResizeHandle.nw) {
                 points = [resizeState.endTransformPoint, resizeRef.element.points[1]];
             }
             if (resizeRef.handle === ResizeHandle.ne) {
-                points = [
-                    [resizeRef.element.points[0][0], resizeState.endTransformPoint[1]],
-                    [resizeState.endTransformPoint[0], resizeRef.element.points[1][1]]
-                ];
+                points = [resizeState.endTransformPoint, [resizeRef.element.points[0][0], resizeRef.element.points[1][1]]];
             }
             if (resizeRef.handle === ResizeHandle.se) {
-                points = [resizeRef.element.points[0], resizeState.endTransformPoint];
+                points = [resizeState.endTransformPoint, resizeRef.element.points[0]];
             }
             if (resizeRef.handle === ResizeHandle.sw) {
-                points = [
-                    [resizeState.endTransformPoint[0], resizeRef.element.points[0][1]],
-                    [resizeRef.element.points[1][0], resizeState.endTransformPoint[1]]
-                ];
+                points = [resizeState.endTransformPoint, [resizeRef.element.points[1][0], resizeRef.element.points[0][1]]];
             }
-            points = normalizeShapePoints(points, isShift);
+            if (isShift) {
+                const rectangle = getRectangleByPoints(points);
+                const factor = points[0][1] > points[1][1] ? 1 : -1;
+                const height = rectangle.width * ratio * factor;
+                points = [[resizeState.endTransformPoint[0], points[1][1] + height], points[1]];
+            }
+            points = normalizeShapePoints(points);
             const { height: textHeight } = PlaitGeometry.getTextManage(resizeRef.element).getSize();
             DrawTransforms.resizeGeometry(board, points, textHeight, resizeRef.path);
         }
