6f04b9302e49dc1b1ec637980b21f3987051dad3
WBbug <1056342711@qq.com>
2023-10-11 14:46:33 +0800

    feat(draw): add some geometry

--- packages/draw/src/utils/engine/index.ts
+++ packages/draw/src/utils/engine/index.ts
@@ -1,9 +1,13 @@
 import { GeometryShape, ShapeEngine } from '../../interfaces';
 import { DiamondEngine } from './diamond';
 import { EllipseEngine } from './ellipse';
+import { LeftArrowEngine } from './left-arrow';
 import { ParallelogramEngine } from './parallelogram';
 import { RectangleEngine } from './rectangle';
+import { RightArrowEngine } from './right-arrow';
 import { RoundRectangleEngine } from './round-rectangle';
+import { TrapezoidEngine } from './trapezoid';
+import { TriangleEngine } from './triangle';
 
 export const ShapeEngineMap: Record<GeometryShape, ShapeEngine> = {
     [GeometryShape.rectangle]: RectangleEngine,
@@ -11,7 +15,11 @@ export const ShapeEngineMap: Record<GeometryShape, ShapeEngine> = {
     [GeometryShape.ellipse]: EllipseEngine,
     [GeometryShape.parallelogram]: ParallelogramEngine,
     [GeometryShape.roundRectangle]: RoundRectangleEngine,
-    [GeometryShape.text]: RectangleEngine
+    [GeometryShape.text]: RectangleEngine,
+    [GeometryShape.triangle]: TriangleEngine,
+    [GeometryShape.leftArrow]: LeftArrowEngine,
+    [GeometryShape.trapezoid]: TrapezoidEngine,
+    [GeometryShape.rightArrow]: RightArrowEngine
 };
 
 export const getEngine = (shape: GeometryShape) => {
---
+++ packages/draw/src/utils/engine/trapezoid.ts
@@ -0,0 +1,45 @@
+import {
+    PlaitBoard,
+    Point,
+    RectangleClient,
+    getNearestPointBetweenPointAndSegments,
+    isPointInPolygon,
+    setStrokeLinecap
+} from '@plait/core';
+import { ShapeEngine } from '../../interfaces';
+import { Options } from 'roughjs/bin/core';
+import { getCenterPointsOnPolygon } from '../geometry';
+
+export const TrapezoidEngine: ShapeEngine = {
+    draw(board: PlaitBoard, rectangle: RectangleClient, options: Options) {
+        const points = getTrapezoidPoints(rectangle);
+        const rs = PlaitBoard.getRoughSVG(board);
+        const polygon = rs.polygon(points, { ...options, fillStyle: 'solid' });
+        setStrokeLinecap(polygon, 'round');
+        return polygon;
+    },
+    isHit(rectangle: RectangleClient, point: Point) {
+        const points = getTrapezoidPoints(rectangle);
+        return isPointInPolygon(point, points);
+    },
+    getNearestPoint(rectangle: RectangleClient, point: Point) {
+        const cornerPoints = getTrapezoidPoints(rectangle);
+        return getNearestPointBetweenPointAndSegments(point, cornerPoints);
+    },
+    getConnectorPoints(rectangle: RectangleClient) {
+        const points = getTrapezoidPoints(rectangle);
+        return getCenterPointsOnPolygon(points);
+    },
+    getCornerPoints(rectangle: RectangleClient) {
+        return getTrapezoidPoints(rectangle);
+    }
+};
+
+export const getTrapezoidPoints = (rectangle: RectangleClient): Point[] => {
+    return [
+        [rectangle.x + rectangle.width * 0.15, rectangle.y],
+        [rectangle.x + rectangle.width * 0.85, rectangle.y],
+        [rectangle.x + rectangle.width, rectangle.y + rectangle.height],
+        [rectangle.x, rectangle.y + rectangle.height]
+    ];
+};
