63f23faa480d8a7655700a1f87aa62fde1c2b2bb
WBbug <1056342711@qq.com>
2023-10-24 17:35:06 +0800

    feat(draw): change name

--- packages/draw/src/constants/pointer.ts
+++ packages/draw/src/constants/pointer.ts
@@ -3,5 +3,5 @@ import { GeometryShape, LineShape } from '../interfaces';
 export type DrawPointerType = GeometryShape | LineShape;
 
 export const getGeometryPointer = () => {
-    return [...Object.keys(GeometryShape), Object.keys(LineShape)];
+    return [...Object.keys(GeometryShape), ...Object.keys(LineShape)];
 };
