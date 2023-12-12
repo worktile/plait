50c268a3f9bfc336664e030c1b58455c6b044455
pubuzhixing8 <pubuzhixing@gmail.com>
2023-12-12 10:24:14 +0800

    feat: add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Never set eux here otherwise it will break the CI
+
+# ALL_PACKAGES
+packages=(
+  "common"
+  "core"
+  "draw"
+  "flow"
+  "layouts"
+  "mind"
+  "text"
+)
+
+npm run build
+
+for package in "${packages[@]}"
+do
+  cd "dist/$package"
+
+  npm publish --access public
+
+  cd ../../
+done
