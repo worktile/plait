df3147105970fb105ed2ea91d8e731ce3fffa107
WBbug <1056342711@qq.com>
2023-12-19 17:00:55 +0800

    feat(draw): use with-resize

--- packages/draw/src/plugins/with-line-text-move.ts
+++ packages/draw/src/plugins/with-line-text-move.ts
@@ -1,76 +1,62 @@
 import {
-    MERGING,
     PlaitBoard,
     PlaitElement,
+    Point,
     distanceBetweenPointAndSegments,
     getHitElementByPoint,
-    getNearestPointBetweenPointAndSegments,
-    preventTouchMove,
-    toPoint,
-    transformPoint
+    getNearestPointBetweenPointAndSegments
 } from '@plait/core';
 import { PlaitDrawElement, PlaitLine } from '../interfaces';
 import { getHitLineTextIndex, getLinePoints } from '../utils';
-import { getRatioByPoint } from '@plait/common';
+import { ResizeHandle, ResizeRef, ResizeState, WithResizeOptions, getRatioByPoint, withResize } from '@plait/common';
 import { DrawTransforms } from '../transforms';
 import { LineComponent } from '../line.component';
 
 export const withLineTextMove = (board: PlaitBoard) => {
-    const { pointerDown, pointerMove, pointerUp } = board;
-
     let textIndex = 0;
-    let element: PlaitLine | null = null;
     const movableBuffer = 100;
-
-    board.pointerDown = (event: PointerEvent) => {
-        if (PlaitBoard.isReadonly(board)) return;
-        const point = transformPoint(board, toPoint(event.x, event.y, PlaitBoard.getHost(board)));
-        const line = getHitElementByPoint(board, point, (element: PlaitElement) => {
-            return PlaitDrawElement.isLine(element);
-        }) as undefined | PlaitLine;
-        if (line) {
-            const index = getHitLineTextIndex(board, line, point);
-            const hitComponent = PlaitElement.getComponent(line) as LineComponent;
-            const textManage = hitComponent.textManages[index];
-            if (index !== -1 && !textManage.isEditing) {
-                element = line;
-                textIndex = index;
-                preventTouchMove(board, event, true);
+    const options: WithResizeOptions<PlaitLine> = {
+        key: 'draw-line',
+        canResize: () => {
+            return true;
+        },
+        detect: (point: Point) => {
+            let result = null;
+            const line = getHitElementByPoint(board, point, (element: PlaitElement) => {
+                return PlaitDrawElement.isLine(element);
+            }) as undefined | PlaitLine;
+            if (line) {
+                const index = getHitLineTextIndex(board, line, point);
+                const hitComponent = PlaitElement.getComponent(line) as LineComponent;
+                const textManage = hitComponent.textManages[index];
+                if (index !== -1 && !textManage.isEditing) {
+                    textIndex = index;
+                    return { element: line, handle: ResizeHandle.e };
+                }
+            }
+            return result;
+        },
+        onResize: (resizeRef: ResizeRef<PlaitLine>, resizeState: ResizeState) => {
+            const element = resizeRef.element;
+            if (element) {
+                const movingPoint = resizeState.endTransformPoint;
+                const points = getLinePoints(board, element);
+                const distance = distanceBetweenPointAndSegments(points, movingPoint);
+                if (distance <= movableBuffer) {
+                    const point = getNearestPointBetweenPointAndSegments(movingPoint, points, false);
+                    const position = getRatioByPoint(points, point);
+                    const texts = [...element.texts];
+                    texts[textIndex] = {
+                        ...texts[textIndex],
+                        position
+                    };
+                    DrawTransforms.setLineTexts(board, element, texts);
+                }
             }
         }
-        pointerDown(event);
     };
 
-    board.pointerMove = (event: PointerEvent) => {
-        if (element) {
-            event.preventDefault();
-            const movingPoint = transformPoint(board, toPoint(event.x, event.y, PlaitBoard.getHost(board)));
-            const points = getLinePoints(board, element);
-            const distance = distanceBetweenPointAndSegments(points, movingPoint);
-            if (distance <= movableBuffer) {
-                const point = getNearestPointBetweenPointAndSegments(movingPoint, points, false);
-                const position = getRatioByPoint(points, point);
-                const texts = [...element.texts];
-                texts[textIndex] = {
-                    ...texts[textIndex],
-                    position
-                };
-                DrawTransforms.setLineTexts(board, element, texts);
-                MERGING.set(board, true);
-            }
-        }
-
-        pointerMove(event);
-    };
-
-    board.pointerUp = (event: PointerEvent) => {
-        if (element) {
-            MERGING.set(board, false);
-            element = null;
-        }
-
-        pointerUp(event);
-    };
+    withResize<PlaitLine>(board, options);
 
     return board;
 };
