81549415f5239f80fdfe4a9bf321b364cb9dc536
pubuzhixing8 <pubuzhixing@gmail.com>
2023-12-25 16:24:14 +0800

    feat(common): add getTextMarksByElement in common

---
+++ .changeset/cuddly-numbers-act.md
@@ -0,0 +1,5 @@
+---
+'@plait/common': minor
+---
+
+add getTextMarksByElement in common
--- packages/common/src/utils/text.ts
+++ packages/common/src/utils/text.ts
@@ -1,5 +1,6 @@
 import { PlaitElement } from '@plait/core';
 import { CommonPluginElement } from '../core/plugin-element';
+import { CustomText, PlaitMarkEditor } from '@plait/text';
 
 export const getTextManages = (element: PlaitElement) => {
     const component = PlaitElement.getComponent(element) as CommonPluginElement;
@@ -10,4 +11,19 @@ export const getTextEditors = (element: PlaitElement) => {
     return getTextManages(element).map(manage => {
         return manage.componentRef.instance.editor;
     });
+};
+
+export const getTextMarksByElement = (element: PlaitElement) => {
+    const editors = getTextEditors(element);
+    const editor = editors[0];
+    if (!editor) {
+        return {};
+    }
+    if (editor.children.length === 0) {
+        const textManage = getTextManages(element)[0];
+        const currentMarks: Omit<CustomText, 'text'> = PlaitMarkEditor.getMarksByElement(textManage.componentRef.instance.children[0]);
+        return currentMarks;
+    }
+    const currentMarks: Omit<CustomText, 'text'> = PlaitMarkEditor.getMarks(editor);
+    return currentMarks;
 };
