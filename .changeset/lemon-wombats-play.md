bb197d93f96b2e6af04bec281eae1e2210ee87ea
huanhuanwa <44698191+huanhuanwa@users.noreply.github.com>
2024-01-26 11:54:40 +0800

    feat(draw): prevent render middle point when source and target intersect #WIK-14445 (#727)

---
+++ .changeset/lemon-wombats-play.md
@@ -0,0 +1,5 @@
+---
+'@plait/draw': patch
+---
+
+prevent render middle point when source and target intersect
--- packages/draw/src/plugins/with-line-resize.ts
+++ packages/draw/src/plugins/with-line-resize.ts
@@ -1,12 +1,12 @@
 import { Path, PlaitBoard, PlaitNode, Point } from '@plait/core';
-import { ResizeRef, ResizeState, WithResizeOptions, simplifyOrthogonalPoints, withResize } from '@plait/common';
+import { ResizeRef, ResizeState, WithResizeOptions, isSourceAndTargetIntersect, simplifyOrthogonalPoints, withResize } from '@plait/common';
 import { getSelectedLineElements } from '../utils/selected';
 import { getHitLineResizeHandleRef, LineResizeHandle } from '../utils/position/line';
 import { getHitOutlineGeometry } from '../utils/position/geometry';
 import { LineHandle, LineShape, PlaitLine } from '../interfaces';
 import { DrawTransforms } from '../transforms';
 import { REACTION_MARGIN } from '../constants';
-import { getElbowPoints, getNextKeyPoints } from '../utils/line/elbow';
+import { getElbowLineRouteOptions, getElbowPoints, getNextKeyPoints } from '../utils/line/elbow';
 import { alignElbowSegment, alignPoints, getIndexAndDeleteCountByKeyPoint, getResizeReferencePoints } from '../utils/line/line-resize';
 import { getConnectionByNearestPoint, getLinePoints } from '../utils/line/line-basic';
 
@@ -42,6 +42,11 @@ export const withLineResize = (board: PlaitBoard) => {
         },
         beforeResize: (resizeRef: ResizeRef<PlaitLine, LineResizeHandle>) => {
             if (resizeRef.element.shape === LineShape.elbow) {
+                const params = getElbowLineRouteOptions(board, resizeRef.element);
+                const isIntersect = isSourceAndTargetIntersect(params);
+                if (isIntersect) {
+                    return;
+                }
                 let points: Point[] = [...resizeRef.element.points];
                 let handleIndex = resizeRef.handleIndex!;
                 const pointsOnElbow = getElbowPoints(board, resizeRef.element);
