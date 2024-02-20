234d6178e86b74e58d66dbc5a5a8514dba33f53a
huanhuanwa <2323666215@qq.com>
2024-02-20 15:30:43 +0800

    fix(draw): optmize code

--- packages/draw/src/utils/line/line-resize.ts
+++ packages/draw/src/utils/line/line-resize.ts
@@ -339,22 +339,23 @@ function findMirrorSegments(
 }
 
 export const hasIllegalPoint = (midDataPoints: Point[]): boolean => {
+    if (midDataPoints.length === 1) {
+        return true;
+    }
     return midDataPoints.some((item, index) => {
-        if (midDataPoints.length === 1) {
-            return true;
-        }
-        if (index > 0 && index < midDataPoints.length - 1) {
+        let isStraightWithBefore: boolean = false;
+        let isStraightWithAfter: boolean = false;
+
+        if (index > 0) {
             const beforePoint = midDataPoints[index - 1];
-            const afterPoint = midDataPoints[index + 1];
-
             const beforeSegment = [beforePoint, item];
-            const afterSegment = [item, afterPoint];
-
-            const isStraightWithBefore = Point.isAlign(beforeSegment);
-            const isStraightWithAfter = Point.isAlign(afterSegment);
-
-            return !isStraightWithBefore && !isStraightWithAfter;
+            isStraightWithBefore = Point.isAlign(beforeSegment);
         }
-        return false;
+        if (index < midDataPoints.length - 1) {
+            const afterPoint = midDataPoints[index + 1];
+            const afterSegment = [item, afterPoint];
+            isStraightWithAfter = Point.isAlign(afterSegment); 
+        }
+        return !isStraightWithBefore && !isStraightWithAfter;
     });
 };
