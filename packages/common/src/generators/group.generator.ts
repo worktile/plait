ac620059e81d21885c34f8427d32eb47531c971b
huanhuanwa <44698191+huanhuanwa@users.noreply.github.com>
2024-03-21 18:42:12 +0800

    fix(common): insert group at the end #WIK-14900 (#784)

--- packages/common/src/generators/group.generator.ts
+++ packages/common/src/generators/group.generator.ts
@@ -1,4 +1,4 @@
-import { ACTIVE_STROKE_WIDTH, PlaitGroup, drawRectangle, getRectangleByGroup } from '@plait/core';
+import { ACTIVE_STROKE_WIDTH, PlaitGroup, RectangleClient, drawRectangle, getRectangleByGroup } from '@plait/core';
 import { Options } from 'roughjs/bin/core';
 import { Generator } from './generator';
 
@@ -13,10 +13,11 @@ export class GroupGenerator extends Generator<PlaitGroup> {
             strokeWidth: ACTIVE_STROKE_WIDTH,
             strokeLineDash: [5]
         };
+        let rectangle: RectangleClient = { x: 0, y: 0, width: 0, height: 0 };
         if (partialSelected) {
             options.stroke = '#999';
+            rectangle = getRectangleByGroup(this.board, element, true);
         }
-        const rectangle = getRectangleByGroup(this.board, element, true);
         return drawRectangle(this.board, rectangle, options);
     }
 }
