d4f44c9930f5c0f8cb1a41fc9a18cc7828320f9d
pubuzhixing8 <pubuzhixing@gmail.com>
2024-04-17 18:42:49 +0800

    Create ci.yml (#830)

--- packages/common/karma.conf.js
+++ packages/common/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/mindmap'),
+      dir: require('path').join(__dirname, '../../coverage/common'),
       subdir: '.',
       reporters: [
         { type: 'html' },
