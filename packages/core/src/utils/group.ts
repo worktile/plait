2010baa014d24d41ff76b67762644709a3ebe9c5
huanhuanwa <44698191+huanhuanwa@users.noreply.github.com>
2024-04-19 17:34:01 +0800

    fix(common): return null when hover selected element #WIK-15199 (#840)

--- packages/core/src/utils/group.ts
+++ packages/core/src/utils/group.ts
@@ -176,13 +176,16 @@ export const getHighestSelectedElements = (board: PlaitBoard, elements?: PlaitEl
 };
 
 export const createGroupRectangleG = (board: PlaitBoard, elements: PlaitElement[]): SVGGElement | null => {
-    const selectedElements = getSelectedElements(board);
-    const groupRectangleG: SVGGElement = createG();
+    const selectedElementIds = getSelectedElements(board).map(item => item.id);
+    let groupRectangleG: SVGGElement | null = null;
     const isMoving = isSelectionMoving(board);
 
     elements.forEach(item => {
-        const isRender = (!selectedElements.includes(item) && !isMoving) || isMoving;
+        const isRender = (!selectedElementIds.includes(item.id) && !isMoving) || isMoving;
         if (item.groupId && isRender) {
+            if (!groupRectangleG) {
+                groupRectangleG = createG();
+            }
             const elements = getElementsInGroupByElement(board, item);
             const rectangle = getRectangleByElements(board, elements, false);
             const rectangleG = drawRectangle(board, rectangle, {
