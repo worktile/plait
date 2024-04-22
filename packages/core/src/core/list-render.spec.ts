a536a32c9ed3bfb290cb6ec63c4194c1ed5a67d6
pubuzhixing8 <pubuzhixing@gmail.com>
2024-04-22 16:50:10 +0800

    chore: add mount element g test structure

---
+++ packages/core/src/core/list-render.spec.ts
@@ -0,0 +1,23 @@
+describe('mountElementG', () => {
+    describe('default', () => {
+        it('container g should have correct position', () => {
+
+        });
+        it('children element should have a root container g', () => {
+
+        });
+        it(`children element's g should have been before of it's parent element container g`, () => {
+
+        });
+        it(`should add to correct position when insert a element`, () => {});
+        it(`should add to correct position when insert a child element`, () => {});
+        it(`should add to correct position when insert a element at start position`, () => {})
+        it(`should add to correct position when insert a child element at start position`, () => {})
+    });
+    describe('move', () => {
+        it('should move container g to correct when move element', () => {});
+        it('should move container g to correct when move element to start position', () => {});
+        it('should move container g to correct when move element to start position and element has been updated', () => {});
+        it('should move multiple container g to correct when move multiple elements to start position', () => {});
+    });
+});
