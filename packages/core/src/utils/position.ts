2868ed7544081425fa94aa88d84cd26f31c73b6a
huanhuanwa <2323666215@qq.com>
2024-04-26 11:43:05 +0800

    fix: restore code

--- packages/core/src/utils/position.ts
+++ packages/core/src/utils/position.ts
@@ -2,8 +2,8 @@ import { PlaitBoard, PlaitElement } from '../interfaces';
 
 export const sortElements = (board: PlaitBoard, elements: PlaitElement[], ascendingOrder = true) => {
     return [...elements].sort((a: PlaitElement, b: PlaitElement) => {
-        const pathA = board.children.findIndex(item=> item.id === a.id);
-        const pathB = board.children.findIndex(item=> item.id === b.id);
-        return ascendingOrder ? pathA - pathB : pathB - pathA;
+        const pathA = PlaitBoard.findPath(board, a);
+        const pathB = PlaitBoard.findPath(board, b);
+        return ascendingOrder ? pathA[0] - pathB[0] : pathB[0] - pathA[0];
     });
 };
