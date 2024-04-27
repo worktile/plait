e0ba1c18a0c3a87578e7d66bb48255c52ce96f5b
pubuzhixing8 <pubuzhixing@gmail.com>
2024-04-27 17:33:16 +0800

    refactor(mind): remove getComponent invoking from image utils

--- packages/mind/src/utils/node/image.ts
+++ packages/mind/src/utils/node/image.ts
@@ -1,19 +1,20 @@
 import { PlaitBoard, PlaitElement } from '@plait/core';
-import { MindNodeComponent } from '../../mind-node.component';
 import { MindElement } from '../../interfaces/element';
 import { ImageData } from '../../interfaces/element-data';
-import { addElementOfFocusedImage, removeElementOfFocusedImage } from '@plait/common';
+import { ImageGenerator, PlaitCommonElementRef, addElementOfFocusedImage, removeElementOfFocusedImage } from '@plait/common';
 
 export const addImageFocus = (board: PlaitBoard, element: MindElement<ImageData>) => {
     addElementOfFocusedImage(board, element);
-    const elementComponent = PlaitElement.getComponent(element) as MindNodeComponent;
-    elementComponent.imageGenerator.componentRef!.instance.isFocus = true;
-    elementComponent.imageGenerator.componentRef!.instance.cdr.markForCheck();
+    const commonElementRef = PlaitElement.getElementRef<PlaitCommonElementRef>(element);
+    const imageGenerator = commonElementRef.getGenerator<ImageGenerator>(ImageGenerator.key);
+    imageGenerator.componentRef!.instance.isFocus = true;
+    imageGenerator.componentRef!.instance.cdr.markForCheck();
 };
 
 export const removeImageFocus = (board: PlaitBoard, element: MindElement<ImageData>) => {
     removeElementOfFocusedImage(board);
-    const elementComponent = PlaitElement.getComponent(element) as MindNodeComponent;
-    elementComponent.imageGenerator.componentRef!.instance.isFocus = false;
-    elementComponent.imageGenerator.componentRef!.instance.cdr.markForCheck();
+    const commonElementRef = PlaitElement.getElementRef<PlaitCommonElementRef>(element);
+    const imageGenerator = commonElementRef.getGenerator<ImageGenerator>(ImageGenerator.key);
+    imageGenerator.componentRef!.instance.isFocus = false;
+    imageGenerator.componentRef!.instance.cdr.markForCheck();
 };
