1d08e793027398bc730b64155fe130b993747d05
huanhuanwa <2323666215@qq.com>
2024-05-28 10:15:26 +0800

    fix: fix can not find text editor

--- src/app/components/setting-panel/setting-panel.component.ts
+++ src/app/components/setting-panel/setting-panel.component.ts
@@ -144,8 +144,10 @@ export class AppSettingPanelComponent extends PlaitIslandBaseComponent implement
         const selectedTableAndGeometryElements = [...selectedGeometryElements, ...selectedTableElements];
         if (selectedTableAndGeometryElements.length) {
             const firstGeometry = selectedTableAndGeometryElements.find(item => isDrawElementIncludeText(item));
-            this.currentMarks = firstGeometry ? PlaitMarkEditor.getMarks(getFirstTextEditor(firstGeometry)) : {};
-            this.align = firstGeometry ? getGeometryAlign(firstGeometry) : Alignment.center;
+            if (firstGeometry && PlaitElement.hasMounted(firstGeometry)) {
+                this.currentMarks = firstGeometry ? PlaitMarkEditor.getMarks(getFirstTextEditor(firstGeometry)) : {};
+                this.align = firstGeometry ? getGeometryAlign(firstGeometry) : Alignment.center;
+            }
             setTimeout(() => {
                 const editor = firstGeometry && getEditingTextEditor(this.board, [firstGeometry]);
                 this.align = (editor?.children[0] as ParagraphElement)?.align || this.align;
