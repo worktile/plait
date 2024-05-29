752c8cde0060829d951f643fc9753dd32eaba926
huanhuanwa <2323666215@qq.com>
2024-05-29 15:40:13 +0800

    fix(common): fix get gap snap line error #WIK-15446

--- packages/core/src/utils/snap/snap-moving.ts
+++ packages/core/src/utils/snap/snap-moving.ts
@@ -151,8 +151,8 @@ function getGapLinesAndDelta(activeRectangle: RectangleClient, snapRectangles: R
             //after
             const distanceRight = after[axis] - (before[axis] + before[side]);
             _center = after[axis] + after[side] + distanceRight + activeRectangle[side] / 2;
-            dif = Math.abs(_center - activeRectangleCenter);
-            if (!gapDistance && dif < SNAP_TOLERANCE) {
+            dif = Math.abs(_center) - Math.abs(activeRectangleCenter);
+            if ((!gapDistance || gapDistance !== distanceRight) && Math.abs(dif) < SNAP_TOLERANCE) {
                 gapDistance = distanceRight;
                 beforeIndex = j;
                 delta = _center - activeRectangleCenter;
@@ -163,7 +163,7 @@ function getGapLinesAndDelta(activeRectangle: RectangleClient, snapRectangles: R
             _center = before[axis] - distanceBefore - activeRectangle[side] / 2;
             dif = Math.abs(_center - activeRectangleCenter);
 
-            if (!gapDistance && dif < SNAP_TOLERANCE) {
+            if ((!gapDistance || gapDistance !== distanceRight) && dif < SNAP_TOLERANCE) {
                 gapDistance = distanceBefore;
                 afterIndex = i;
                 delta = _center - activeRectangleCenter;
