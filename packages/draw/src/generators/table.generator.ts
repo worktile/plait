42aa8299adc77cfc72595f8d3dbef52d9c776a09
huanhuanwa <2323666215@qq.com>
2024-06-19 16:08:24 +0800

    feat(draw): support set border style for table border #WIK-15798

--- packages/draw/src/generators/table.generator.ts
+++ packages/draw/src/generators/table.generator.ts
@@ -3,7 +3,7 @@ import { Generator } from '@plait/common';
 import { PlaitElement, RectangleClient } from '@plait/core';
 import { PlaitBaseTable } from '../interfaces/table';
 import { getEngine } from '../engines';
-import { getDrawDefaultStrokeColor } from '../utils';
+import { getDrawDefaultStrokeColor, getFillByElement, getLineDashByElement, getStrokeColorByElement, getStrokeWidthByElement } from '../utils';
 
 export interface TableData {}
 
@@ -14,12 +14,16 @@ export class TableGenerator<T extends PlaitElement = PlaitBaseTable> extends Gen
 
     draw(element: T, data: TableData) {
         const rectangle = RectangleClient.getRectangleByPoints(element.points!);
+        const strokeLineDash = getLineDashByElement(element);
+        const strokeWidth = getStrokeWidthByElement(element);
+        const strokeColor = getStrokeColorByElement(this.board, element);
         return getEngine(TableSymbols.table).draw(
             this.board,
             rectangle,
             {
-                strokeWidth: 2,
-                stroke: getDrawDefaultStrokeColor(this.board.theme.themeColorMode),
+                strokeWidth,
+                stroke: strokeColor,
+                strokeLineDash,
             },
             {
                 element: element
