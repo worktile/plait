ccf6f718871549d411e265c5ab6389c5d1cadeda
pubuzhixing8 <pubuzhixing@gmail.com>
2025-01-01 18:14:25 +0800

    fix(draw): complete provided-interface intersection

--- packages/draw/src/engines/uml/provided-interface.ts
+++ packages/draw/src/engines/uml/provided-interface.ts
@@ -3,34 +3,53 @@ import {
     Point,
     PointOfRectangle,
     RectangleClient,
+    distanceBetweenPointAndPoint,
     getEllipseTangentSlope,
+    getNearestPointBetweenPointAndEllipse,
     getNearestPointBetweenPointAndSegments,
     getVectorFromPointAndSlope,
     setStrokeLinecap
 } from '@plait/core';
 import { ShapeEngine } from '../../interfaces';
 import { Options } from 'roughjs/bin/core';
-import { RectangleEngine } from '../basic-shapes/rectangle';
 import { getUnitVectorByPointAndPoint } from '@plait/common';
+
+const percentage = 0.54;
+
+export const getStartPoint = (rectangle: RectangleClient): Point => {
+    return [rectangle.x, rectangle.y + rectangle.height / 2];
+};
+
+export const getEndPoint = (rectangle: RectangleClient): Point => {
+    return [rectangle.x + rectangle.width * percentage, rectangle.y + rectangle.height / 2];
+};
+
+export const arcPercentage = percentage + (1 - percentage) / 2;
+
+export const getArcCenter = (rectangle: RectangleClient): Point => {
+    return [rectangle.x + arcPercentage * rectangle.width, rectangle.y + rectangle.height / 2];
+};
 
 export const ProvidedInterfaceEngine: ShapeEngine = {
     draw(board: PlaitBoard, rectangle: RectangleClient, options: Options) {
         const rs = PlaitBoard.getRoughSVG(board);
+        const startPoint = getStartPoint(rectangle);
+        const endPoint = getEndPoint(rectangle);
         const shape = rs.path(
-            ` M${rectangle.x} ${rectangle.y + rectangle.height / 2} 
-        H${rectangle.x + rectangle.width * 0.54}
-        A${(rectangle.width * 0.46) / 2} ${rectangle.height / 2}, 0, 1, 1 ${rectangle.x + rectangle.width} ${rectangle.y +
-                rectangle.height / 2}
-        A${(rectangle.width * 0.46) / 2} ${rectangle.height / 2}, 0, 1, 1 ${rectangle.x + rectangle.width * 0.54} ${rectangle.y +
-                rectangle.height / 2}
-        `,
+            `M${startPoint[0]} ${startPoint[1]} 
+        H${endPoint[0]}
+        A${(rectangle.width * (1 - percentage)) / 2} ${rectangle.height / 2}, 0, 1, 1 ${rectangle.x + rectangle.width} ${
+                rectangle.y + rectangle.height / 2
+            }
+        A${(rectangle.width * (1 - percentage)) / 2} ${rectangle.height / 2}, 0, 1, 1 ${rectangle.x + rectangle.width * percentage} ${
+                rectangle.y + rectangle.height / 2
+            }`,
             {
                 ...options,
                 fillStyle: 'solid'
             }
         );
         setStrokeLinecap(shape, 'round');
-
         return shape;
     },
     isInsidePoint(rectangle: RectangleClient, point: Point) {
@@ -44,8 +63,22 @@ export const ProvidedInterfaceEngine: ShapeEngine = {
         return RectangleClient.getEdgeCenterPoints(rectangle);
     },
     getNearestPoint(rectangle: RectangleClient, point: Point) {
-        const nearestPoint = getNearestPointBetweenPointAndSegments(point, RectangleEngine.getCornerPoints(rectangle));
-        return nearestPoint;
+        const startPoint = getStartPoint(rectangle);
+        const endPoint = getEndPoint(rectangle);
+        const nearestPointForLine = getNearestPointBetweenPointAndSegments(point, [startPoint, endPoint]);
+        const distanceForLine = distanceBetweenPointAndPoint(...point, ...nearestPointForLine);
+        const arcCenter = getArcCenter(rectangle);
+        const nearestPointForEllipse = getNearestPointBetweenPointAndEllipse(
+            point,
+            arcCenter,
+            (rectangle.width * (1 - percentage)) / 2,
+            rectangle.height / 2
+        );
+        const distanceForEllipse = distanceBetweenPointAndPoint(...point, ...nearestPointForEllipse);
+        if (distanceForLine < distanceForEllipse) {
+            return nearestPointForLine;
+        }
+        return nearestPointForEllipse;
     },
     getTangentVectorByConnectionPoint(rectangle: RectangleClient, pointOfRectangle: PointOfRectangle) {
         const connectionPoint = RectangleClient.getConnectionPoint(rectangle, pointOfRectangle);
