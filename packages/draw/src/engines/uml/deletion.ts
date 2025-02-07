afebea67ea3e88f352c6aa93413a8dadf4cd8fae
pubuzhixing8 <pubuzhixing@gmail.com>
2025-02-07 15:08:27 +0800

    feat(deletion): improve deletion hitting check #1013

--- packages/draw/src/engines/uml/deletion.ts
+++ packages/draw/src/engines/uml/deletion.ts
@@ -3,6 +3,8 @@ import {
     Point,
     PointOfRectangle,
     RectangleClient,
+    distanceBetweenPointAndPoint,
+    getNearestPointBetweenPointAndSegment,
     getNearestPointBetweenPointAndSegments,
     setStrokeLinecap
 } from '@plait/core';
@@ -11,13 +13,25 @@ import { Options } from 'roughjs/bin/core';
 import { getPolygonEdgeByConnectionPoint } from '../../utils/polygon';
 import { RectangleEngine } from '../basic-shapes/rectangle';
 
+function getDeletionLines(rectangle: RectangleClient): Array<[Point, Point]> {
+    return [
+        [
+            [rectangle.x, rectangle.y],
+            [rectangle.x + rectangle.width, rectangle.y + rectangle.height]
+        ],
+        [
+            [rectangle.x + rectangle.width, rectangle.y],
+            [rectangle.x, rectangle.y + rectangle.height]
+        ]
+    ];
+}
+
 export const DeletionEngine: ShapeEngine = {
     draw(board: PlaitBoard, rectangle: RectangleClient, options: Options) {
         const rs = PlaitBoard.getRoughSVG(board);
+        const lines = getDeletionLines(rectangle);
         const shape = rs.path(
-            `M${rectangle.x} ${rectangle.y} L${rectangle.x + rectangle.width} ${rectangle.y + rectangle.height} 
-            M${rectangle.x + rectangle.width} ${rectangle.y} L${rectangle.x} ${rectangle.y + rectangle.height} 
-            `,
+            lines.map(([from, to]) => `M${from[0]} ${from[1]} L${to[0]} ${to[1]}`).join(' '),
             { ...options, fillStyle: 'solid', strokeWidth: 4 }
         );
         setStrokeLinecap(shape, 'round');
@@ -31,7 +45,18 @@ export const DeletionEngine: ShapeEngine = {
         return RectangleClient.getCornerPoints(rectangle);
     },
     getNearestPoint(rectangle: RectangleClient, point: Point) {
-        return getNearestPointBetweenPointAndSegments(point, RectangleEngine.getCornerPoints(rectangle));
+        const lines = getDeletionLines(rectangle);
+        let minDistance = Infinity;
+        let nearestPoint = point;
+        lines.forEach(line => {
+            const currentPoint = getNearestPointBetweenPointAndSegment(point, line);
+            const distance = distanceBetweenPointAndPoint(point[0], point[1], currentPoint[0], currentPoint[1]);
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestPoint = currentPoint;
+            }
+        });
+        return nearestPoint;
     },
     getEdgeByConnectionPoint(rectangle: RectangleClient, pointOfRectangle: PointOfRectangle): [Point, Point] | null {
         const corners = RectangleEngine.getCornerPoints(rectangle);
