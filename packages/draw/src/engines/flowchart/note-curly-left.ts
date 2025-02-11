91f9fe4760024fb3d1428241a5361b462edf9de5
pubuzhixing8 <pubuzhixing@gmail.com>
2025-02-11 10:10:08 +0800

    feat: improve note-curly-left and note-curly-left logic #1017 #1018

--- packages/draw/src/engines/flowchart/note-curly-left.ts
+++ packages/draw/src/engines/flowchart/note-curly-left.ts
@@ -3,6 +3,7 @@ import {
     Point,
     PointOfRectangle,
     RectangleClient,
+    distanceBetweenPointAndPoint,
     getNearestPointBetweenPointAndSegments,
     setStrokeLinecap
 } from '@plait/core';
@@ -12,22 +13,89 @@ import { Options } from 'roughjs/bin/core';
 import { RectangleEngine } from '../basic-shapes/rectangle';
 import { getPolygonEdgeByConnectionPoint } from '../../utils/polygon';
 import { getStrokeWidthByElement } from '../../utils';
+import { pointsOnBezierCurves } from 'points-on-curve';
+
+interface NoteCurlyLeftPathData {
+    startPoint: Point;
+    upperCurve: {
+        controlPoint1: Point;
+        controlPoint2: Point;
+        endPoint: Point;
+    };
+    lowerCurve: {
+        controlPoint1: Point;
+        controlPoint2: Point;
+        endPoint: Point;
+    };
+}
+
+function generateNoteCurlyLeftPath(rectangle: RectangleClient): NoteCurlyLeftPathData {
+    const curlyWidth = rectangle.width * 0.09;
+    const rightX = rectangle.x + rectangle.width;
+    const centerY = rectangle.y + rectangle.height / 2;
+
+    return {
+        startPoint: [rightX, rectangle.y],
+        upperCurve: {
+            controlPoint1: [rightX - curlyWidth, rectangle.y],
+            controlPoint2: [rightX, centerY],
+            endPoint: [rightX - curlyWidth, centerY]
+        },
+        lowerCurve: {
+            controlPoint1: [rightX, centerY],
+            controlPoint2: [rightX - curlyWidth, rectangle.y + rectangle.height],
+            endPoint: [rightX, rectangle.y + rectangle.height]
+        }
+    };
+}
 
 export const NoteCurlyLeftEngine: ShapeEngine = {
     draw(board: PlaitBoard, rectangle: RectangleClient, options: Options) {
         const rs = PlaitBoard.getRoughSVG(board);
-        const shape = rs.path(
-            `M${rectangle.x + rectangle.width} ${rectangle.y} 
-            C${rectangle.x + rectangle.width - rectangle.width * 0.09} ${rectangle.y}, 
-            ${rectangle.x + rectangle.width} ${rectangle.y + rectangle.height / 2}, 
-            ${rectangle.x + rectangle.width - rectangle.width * 0.09} ${rectangle.y + rectangle.height / 2}
-            C${rectangle.x + rectangle.width} ${rectangle.y + rectangle.height / 2}, 
-            ${rectangle.x + rectangle.width - rectangle.width * 0.09} ${rectangle.y + rectangle.height}, 
-            ${rectangle.x + rectangle.width} ${rectangle.y + rectangle.height}`,
-            { ...options, fillStyle: 'solid', fill: 'transparent' }
-        );
+        const { startPoint, upperCurve, lowerCurve } = generateNoteCurlyLeftPath(rectangle);
+
+        const pathData = [
+            `M${startPoint[0]} ${startPoint[1]}`,
+            `C${upperCurve.controlPoint1[0]} ${upperCurve.controlPoint1[1]},
+            ${upperCurve.controlPoint2[0]} ${upperCurve.controlPoint2[1]},
+            ${upperCurve.endPoint[0]} ${upperCurve.endPoint[1]}`,
+            `C${lowerCurve.controlPoint1[0]} ${lowerCurve.controlPoint1[1]},
+            ${lowerCurve.controlPoint2[0]} ${lowerCurve.controlPoint2[1]},
+            ${lowerCurve.endPoint[0]} ${lowerCurve.endPoint[1]}`
+        ].join(' ');
+
+        const shape = rs.path(pathData, { ...options, fillStyle: 'solid', fill: 'transparent' });
         setStrokeLinecap(shape, 'round');
         return shape;
+    },
+
+    getNearestPoint(rectangle: RectangleClient, point: Point) {
+        const { startPoint, upperCurve, lowerCurve } = generateNoteCurlyLeftPath(rectangle);
+
+        const upperBezierPoints = pointsOnBezierCurves(
+            [startPoint, upperCurve.controlPoint1, upperCurve.controlPoint2, upperCurve.endPoint],
+            0.001
+        );
+
+        const lowerBezierPoints = pointsOnBezierCurves(
+            [upperCurve.endPoint, lowerCurve.controlPoint1, lowerCurve.controlPoint2, lowerCurve.endPoint],
+            0.001
+        );
+
+        const allPoints = [...upperBezierPoints, ...lowerBezierPoints];
+
+        let minDistance = Infinity;
+        let nearestPoint = point;
+
+        for (const curvePoint of allPoints) {
+            const distance = distanceBetweenPointAndPoint(point[0], point[1], curvePoint[0], curvePoint[1]);
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestPoint = [...curvePoint];
+            }
+        }
+
+        return nearestPoint;
     },
     isInsidePoint(rectangle: RectangleClient, point: Point) {
         const rangeRectangle = RectangleClient.getRectangleByPoints([point, point]);
@@ -35,9 +103,6 @@ export const NoteCurlyLeftEngine: ShapeEngine = {
     },
     getCornerPoints(rectangle: RectangleClient) {
         return RectangleClient.getCornerPoints(rectangle);
-    },
-    getNearestPoint(rectangle: RectangleClient, point: Point) {
-        return getNearestPointBetweenPointAndSegments(point, RectangleEngine.getCornerPoints(rectangle));
     },
     getEdgeByConnectionPoint(rectangle: RectangleClient, pointOfRectangle: PointOfRectangle): [Point, Point] | null {
         const corners = RectangleEngine.getCornerPoints(rectangle);
--- packages/draw/src/engines/flowchart/note-curly-right.ts
+++ packages/draw/src/engines/flowchart/note-curly-right.ts
@@ -3,6 +3,7 @@ import {
     Point,
     PointOfRectangle,
     RectangleClient,
+    distanceBetweenPointAndPoint,
     getNearestPointBetweenPointAndSegments,
     setStrokeLinecap
 } from '@plait/core';
@@ -12,20 +13,57 @@ import { Options } from 'roughjs/bin/core';
 import { RectangleEngine } from '../basic-shapes/rectangle';
 import { getPolygonEdgeByConnectionPoint } from '../../utils/polygon';
 import { getStrokeWidthByElement } from '../../utils';
+import { pointsOnBezierCurves } from 'points-on-curve';
+
+interface NoteCurlyRightPathData {
+    startPoint: Point;
+    upperCurve: {
+        controlPoint1: Point;
+        controlPoint2: Point;
+        endPoint: Point;
+    };
+    lowerCurve: {
+        controlPoint1: Point;
+        controlPoint2: Point;
+        endPoint: Point;
+    };
+}
+
+function generateNoteCurlyRightPath(rectangle: RectangleClient): NoteCurlyRightPathData {
+    const curlyWidth = rectangle.width * 0.09;
+    const centerY = rectangle.y + rectangle.height / 2;
+
+    return {
+        startPoint: [rectangle.x, rectangle.y],
+        upperCurve: {
+            controlPoint1: [rectangle.x + curlyWidth, rectangle.y],
+            controlPoint2: [rectangle.x, centerY],
+            endPoint: [rectangle.x + curlyWidth, centerY]
+        },
+        lowerCurve: {
+            controlPoint1: [rectangle.x, centerY],
+            controlPoint2: [rectangle.x + curlyWidth, rectangle.y + rectangle.height],
+            endPoint: [rectangle.x, rectangle.y + rectangle.height]
+        }
+    };
+}
 
 export const NoteCurlyRightEngine: ShapeEngine = {
     draw(board: PlaitBoard, rectangle: RectangleClient, options: Options) {
         const rs = PlaitBoard.getRoughSVG(board);
-        const shape = rs.path(
-            `M${rectangle.x} ${rectangle.y}
-            C${rectangle.x + rectangle.width * 0.09} ${rectangle.y},
-            ${rectangle.x} ${rectangle.y + rectangle.height / 2},
-            ${rectangle.x + rectangle.width * 0.09} ${rectangle.y + rectangle.height / 2}
-            C${rectangle.x} ${rectangle.y + rectangle.height / 2},
-            ${rectangle.x + rectangle.width * 0.09} ${rectangle.y + rectangle.height},
-            ${rectangle.x} ${rectangle.y + rectangle.height}`,
-            { ...options, fillStyle: 'solid', fill: 'transparent' }
-        );
+        const { startPoint, upperCurve, lowerCurve } = generateNoteCurlyRightPath(rectangle);
+
+        const pathData = [
+            `M${startPoint[0]} ${startPoint[1]}`,
+            `C${upperCurve.controlPoint1[0]} ${upperCurve.controlPoint1[1]},
+            ${upperCurve.controlPoint2[0]} ${upperCurve.controlPoint2[1]},
+            ${upperCurve.endPoint[0]} ${upperCurve.endPoint[1]}`,
+            `C${lowerCurve.controlPoint1[0]} ${lowerCurve.controlPoint1[1]},
+            ${lowerCurve.controlPoint2[0]} ${lowerCurve.controlPoint2[1]},
+            ${lowerCurve.endPoint[0]} ${lowerCurve.endPoint[1]}`
+        ].join(' ');
+
+        const shape = rs.path(pathData, { ...options, fillStyle: 'solid', fill: 'transparent' });
         setStrokeLinecap(shape, 'round');
         return shape;
     },
@@ -37,7 +75,36 @@ export const NoteCurlyRightEngine: ShapeEngine = {
         return RectangleClient.getCornerPoints(rectangle);
     },
     getNearestPoint(rectangle: RectangleClient, point: Point) {
-        return getNearestPointBetweenPointAndSegments(point, RectangleEngine.getCornerPoints(rectangle));
+        const { startPoint, upperCurve, lowerCurve } = generateNoteCurlyRightPath(rectangle);
+
+        // 生成上部贝塞尔曲线的点
+        const upperBezierPoints = pointsOnBezierCurves(
+            [startPoint, upperCurve.controlPoint1, upperCurve.controlPoint2, upperCurve.endPoint],
+            0.001
+        );
+
+        // 生成下部贝塞尔曲线的点
+        const lowerBezierPoints = pointsOnBezierCurves(
+            [upperCurve.endPoint, lowerCurve.controlPoint1, lowerCurve.controlPoint2, lowerCurve.endPoint],
+            0.001
+        );
+
+        // 合并所有点
+        const allPoints = [...upperBezierPoints, ...lowerBezierPoints];
+
+        // 找到最近的点
+        let minDistance = Infinity;
+        let nearestPoint = [...point] as Point;
+
+        for (const curvePoint of allPoints) {
+            const distance = distanceBetweenPointAndPoint(point[0], point[1], curvePoint[0], curvePoint[1]);
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestPoint = [...curvePoint];
+            }
+        }
+
+        return nearestPoint;
     },
     getEdgeByConnectionPoint(rectangle: RectangleClient, pointOfRectangle: PointOfRectangle): [Point, Point] | null {
         const corners = RectangleEngine.getCornerPoints(rectangle);
